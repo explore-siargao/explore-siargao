702848a7931c63a8bf6e6ed2f2a9fc1c328dc6b0
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-17 11:46:39 +0800

    refactor(api): changes app to es

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import cors from 'cors'
-import { port, origins } from './config'
+import { port } from './config'
 import dotenv from 'dotenv'
 import mysql from 'mysql2'
 dotenv.config()
 
 const connection = mysql.createConnection(process.env.DATABASE_URL || '')
-const app = express()
-app.use(express.json())
-app.use(
+const es = express()
+es.use(express.json())
+es.use(
   cors({
     origin: process.env.ORIGINS,
     credentials: true,
@@ -17,6 +17,6 @@ app.use(
 
 connection.connect((err: any) => console.log('PlannetScale Connected'))
 
-app.listen(port, () => {
+es.listen(port, () => {
   console.log(`🚀 SERVER is running at http://localhost:${port}`)
 })
