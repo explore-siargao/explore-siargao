ecfa4789c80facf89377df083470c9a04fef64ff
jazer <albalate.jazer@gmail.com>
2023-11-17 16:46:25 +0800

    refactor(web): Fix the recommended smell code

--- apps/web/app/component/pages/signup/Content.tsx
+++ apps/web/app/component/pages/signup/Content.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import logo from "../../../public/esLogo.png"
 import Image from "next/image"
-
+import fb from "../../../public/facebook-logo.png"
+import google from "../../../public/google-logo.png"
 const Content = () => {
   return (
     <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-slate-200">
@@ -15,7 +16,6 @@ const Content = () => {
               height={500}
               alt="ES-Logo"
             />
-
             <h2 className="my-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign up to Explore Siargao
             </h2>
@@ -26,34 +26,30 @@ const Content = () => {
                 href="#"
                 className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                  fill="none"
-                  role="button"
-                >
-                  <path
-                    d="M17.64 9.20419C17.64 8.56601 17.5827 7.95237 17.4764 7.36328H9V10.8446H13.8436C13.635 11.9696 13.0009 12.9228 12.0477 13.561V15.8192H14.9564C16.6582 14.2524 17.64 11.9451 17.64 9.20419Z"
-                    fill="#4285F4"
-                  ></path>
-                  <path
-                    d="M8.99976 18C11.4298 18 13.467 17.1941 14.9561 15.8195L12.0475 13.5613C11.2416 14.1013 10.2107 14.4204 8.99976 14.4204C6.65567 14.4204 4.67158 12.8372 3.96385 10.71H0.957031V13.0418C2.43794 15.9831 5.48158 18 8.99976 18Z"
-                    fill="#34A853"
-                  ></path>
-                  <path
-                    d="M3.96409 10.7098C3.78409 10.1698 3.68182 9.59301 3.68182 8.99983C3.68182 8.40664 3.78409 7.82983 3.96409 7.28983V4.95801H0.957273C0.347727 6.17301 0 7.54755 0 8.99983C0 10.4521 0.347727 11.8266 0.957273 13.0416L3.96409 10.7098Z"
-                    fill="#FBBC05"
-                  ></path>
-                  <path
-                    clip-rule="evenodd"
-                    d="M8.99976 3.57955C10.3211 3.57955 11.5075 4.03364 12.4402 4.92545L15.0216 2.34409C13.4629 0.891818 11.4257 0 8.99976 0C5.48158 0 2.43794 2.01682 0.957031 4.95818L3.96385 7.29C4.67158 5.16273 6.65567 3.57955 8.99976 3.57955Z"
-                    fill="#EA4335"
-                  ></path>
-                </svg>
+                <Image
+                  className="h-5 w-auto"
+                  src={google}
+                  width={500}
+                  height={500}
+                  alt="fb-Logo"
+                />
                 <span className="text-sm font-semibold leading-6">
                   Sign up with Google
+                </span>
+              </a>
+              <a
+                href="#"
+                className="flex w-full items-center justify-center gap-3 rounded-md bg-[#5862ba] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+              >
+                <Image
+                  className="h-5 w-auto"
+                  src={fb}
+                  width={500}
+                  height={500}
+                  alt="fb-Logo"
+                />
+                <span className="text-sm font-semibold leading-6">
+                  Sign up with Facebook
                 </span>
               </a>
             </div>
@@ -100,7 +96,6 @@ const Content = () => {
                 />
               </div>
             </div>
-
             <div>
               <label
                 htmlFor="email"
@@ -214,5 +209,4 @@ const Content = () => {
     </div>
   )
 }
-
 export default Content
