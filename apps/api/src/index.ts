5e0c289ea8c1287493e2dc50e07f3ddeee919218
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-17 10:54:22 +0800

    feat(web): Connected to plannetScale

---
+++ apps/api/src/index.ts
@@ -0,0 +1,26 @@
+import express, { Application } from 'express'
+import cors from 'cors'
+import { port, origins } from './config'
+import dotenv from 'dotenv'
+import mysql from 'mysql2'
+import routes from './routes'
+import { loadAndSaveData } from './seeds/seed'
+dotenv.config()
+
+const connection = mysql.createConnection(process.env.DATABASE_URL || '')
+const app: Application = express()
+app.use(express.json())
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+)
+
+routes(app)
+
+connection.connect((err: any) => console.log('PlannetScale Connected'))
+
+app.listen(port, () => {
+  console.log(`🚀 SERVER is running at http://localhost:${port}`)
+})
