42a1ea5fe41b57eddbe9f18c506b9aa3f35d5eca
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-23 10:56:38 +0800

    feat(web): Created login container

---
+++ apps/web/common/components/ui/LoginContainer.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import {
+  ChevronLeftIcon,
+  EnvelopeIcon,
+  XMarkIcon,
+} from "@heroicons/react/20/solid";
+type Props = {
+  children: React.ReactNode;
+  title: string;
+};
+
+const LoginContainer = ({ children, title }: Props) => (
+    <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-slate-200">
+      <div className="sm:mx-auto sm:w-full sm:max-w-[540px]">
+        <div className="bg-white shadow sm:rounded-2xl py-6">
+          <div className="flex border-b-gray-400/50 border-b pb-4 px-4">
+            <XMarkIcon className="h-6 w-6  rounded-full hover:bg-gray-300/30" />
+            <h1 className="w-full text-center place-self-center font-semibold">
+              {title}
+            </h1>
+          </div>
+          <div className="px-6 pt-2">
+          {children}
+          </div>
+        </div>
+      </div>
+    </div>
+);
+
+export default LoginContainer;
