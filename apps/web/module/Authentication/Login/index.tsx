41f7d17783b1de5cc408fa305125014b29be8080
jazer <albalate.jazer@gmail.com>
2023-11-23 15:27:53 +0800

    refactor(web): Fix the code smell

--- apps/web/module/Authentication/Login/index.tsx
+++ apps/web/module/Authentication/Login/index.tsx
@@ -1,28 +1,14 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import LoginInputs from "@/common/components/ui/LoginInputs";
 import AuthWrapper from "@/common/components/ui/AuthWrapper";
 import SignupInputs from "@/common/components/ui/SignupInputs";
 
 const Login = () => {
-  const [modalState, setModalState] = useState(0);
-
-  const renderLoginSignup = () => {
-    return (
-      <AuthWrapper title="Login or sign up" closable={false} isLoggedIn={true}>
-        <LoginInputs modalState={modalState} />
-      </AuthWrapper>
-    );
-  };
-  const renderContinueWithEmail = () => {
-    return (
-      <AuthWrapper title="Sign up" closable={false} isLoggedIn={false}>
-        <SignupInputs modalState={modalState} />
-      </AuthWrapper>
-    );
-  };
   return (
-    <>{modalState === 0 ? renderLoginSignup() : renderContinueWithEmail()}</>
+    <AuthWrapper title="Login or sign up" closable={false} isLoggedIn={true}>
+      <LoginInputs />
+    </AuthWrapper>
   );
 };
 
