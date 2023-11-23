72a4ddad0b253eb4e99fe89553855d2fdc269d70
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-23 11:00:10 +0800

    feat(web): Created login container

--- apps/web/common/components/ui/LoginContainer.tsx
+++ apps/web/common/components/ui/LoginContainer.tsx
@@ -1,10 +1,6 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import {
-  ChevronLeftIcon,
-  EnvelopeIcon,
   XMarkIcon,
 } from "@heroicons/react/20/solid";
 type Props = {
