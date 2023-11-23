b514eb0088052d743783989075167f9001a42794
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-23 16:58:51 +0800

    fix(web): Fixed from log in to sign up transition

--- apps/web/common/components/ui/AuthWrapper.tsx
+++ apps/web/common/components/ui/AuthWrapper.tsx
@@ -12,19 +12,26 @@ type Props = {
 const AuthWrapper = ({ children, title, closable, isLoggedIn }: Props) => {
   const setLogin = useAuthModalStore((state:any)=>state.setIsLogin)
 const setClosable = useAuthModalStore((state:any)=>state.setClosable)
+const closableAndIsLogin =()=>{
+  if(closable){
+    if(isLoggedIn){
+      return <XMarkIcon className="h-6 w-6  rounded-full hover:bg-gray-300/30" />
+    }else{
+return <ChevronLeftIcon onClick={()=>{
+      setLogin()
+      setClosable(false)
+    }} className="h-6 w-6  rounded-full hover:bg-gray-300/30" />
+    }
+  }else{
+    return ""
+  }
+}
 return(
   <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-slate-200">
     <div className="sm:mx-auto sm:w-full sm:max-w-[540px]">
       <div className="bg-white shadow sm:rounded-2xl py-6">
         <div className="flex border-b-gray-400/50 border-b pb-4 px-4">
-          {closable ? isLoggedIn ?(
-            <XMarkIcon className="h-6 w-6  rounded-full hover:bg-gray-300/30" />
-          ) : (
-            <ChevronLeftIcon onClick={()=>{
-              setLogin()
-              setClosable(false)
-            }} className="h-6 w-6  rounded-full hover:bg-gray-300/30" />
-          ):("")}
+          {closableAndIsLogin()}
           <h1 className="w-full text-center place-self-center font-semibold">
             {title}
           </h1>
