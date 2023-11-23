4b010e23e3a563b290e0a70ed7ef1a5a0059a5b9
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-23 09:52:07 +0800

    hotfix(web): Change folder structure

--- packages/ui/src/code.tsx
+++ packages/ui/src/code.tsx
@@ -2,7 +2,7 @@ export function Code({
   children,
   className,
 }: {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
   readonly className?: string;
 }): JSX.Element {
   return <code className={className}>{children}</code>;
