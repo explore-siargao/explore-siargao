d190a472425a0c8105545f29f3b378426dd64d54
jpmadrigal07 <jpmadrigal07@users.noreply.github.com>
2023-11-26 07:05:10 +0000

    Prettified Code!

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -4,6 +4,6 @@ module.exports = {
   extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ['./tsconfig.json'],
+    project: ["./tsconfig.json"],
   },
-};
+}
