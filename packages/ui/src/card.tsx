9803c1adf66e3ecbef2bb68f7a0128c1edbe21d3
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-26 14:23:51 +0800

    refactor: Test precommit 6

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -12,14 +12,14 @@ export function Card({
   return (
     <a
       className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
+        href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
       rel="noopener noreferrer"
       target="_blank"
     >
-      <h2>
-        {title} <span>-mekusmekus</span>
+         <h2>
+        {title} <span>-&gt;</span>
       </h2>
-      <p>{children}</p>
+          <p>{children}</p>
     </a>
   );
 }
