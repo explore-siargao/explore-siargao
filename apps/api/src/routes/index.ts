b56eb5ac92c5d6419ba7264de5f184c6d569250d
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-26 09:33:54 +0800

    refactor: WIP for fixing api setup

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Application } from 'express'
 import UsersRoute from '@/routes/users'
-import { API_ROOT } from '@/common/constants'
+import { API_ROOT_ROOT } from '@repo/constants'
 
 export default function (app: Application) {
-  app.use(`${API_ROOT}/users`, UsersRoute)
+  app.use(`${API_ROOT_ROOT}/users`, UsersRoute)
 }
