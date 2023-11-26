87ebc046f3774ffaf08ac39c691384e8adf9531b
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-11-26 14:26:25 +0800

    refactor: Test precommit 7

--- apps/web/module/Authentication/CreateAccount/index.tsx
+++ apps/web/module/Authentication/CreateAccount/index.tsx
@@ -2,16 +2,16 @@
 import React from "react";
 import AuthContainer from "@/common/components/AuthContainer";
 import SignupInputs from "@/common/components/SignupInputs";
-import { useRouter } from "next/navigation";
+import {   useRouter } from "next/navigation";
 
 const CreateAccount = () => {
   const router = useRouter()
   return (
     <AuthContainer
       title="Create account using you email"
-      onBack={() => router.push("/")}
+          onBack={() => router.push("/")}
     >
-      <SignupInputs />
+          <SignupInputs />
     </AuthContainer>
   );
 };
