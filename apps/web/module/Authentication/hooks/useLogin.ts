c09cebd7e6947c0133e6c9e5ce2633b84c3b73f4
albalatejazer <albalatejazer@users.noreply.github.com>
2023-11-29 07:02:55 +0000

    Prettified Code!

--- apps/web/module/Authentication/hooks/useLogin.ts
+++ apps/web/module/Authentication/hooks/useLogin.ts
@@ -1,10 +1,10 @@
 import { API_URL_USERS } from "@repo/constants"
 import { I_User } from "@/common/types/global"
 import { useMutation } from "@tanstack/react-query"
-import { ApiService } from "@/common/service/api";
+import { ApiService } from "@/common/service/api"
 
 export async function loginUser({ email, password }: I_User) {
-  const apiService = new ApiService();
+  const apiService = new ApiService()
   return await apiService.post(`${API_URL_USERS}/login`, {
     email,
     password,
