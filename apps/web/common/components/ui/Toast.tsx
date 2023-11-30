dd4b4949ce532d9b18cf1fc81425dc33923cc930
jazer <albalate.jazer@gmail.com>
2023-11-30 15:55:58 +0800

    refactor(web): Fix bug on code smell

--- apps/web/common/components/ui/Toast.tsx
+++ apps/web/common/components/ui/Toast.tsx
@@ -5,6 +5,7 @@ import {
   CheckCircleIcon,
   ExclamationCircleIcon,
   ExclamationTriangleIcon,
+  QuestionMarkCircleIcon,
   XMarkIcon,
 } from "@heroicons/react/20/solid"
 
@@ -61,13 +62,19 @@ const Toast = React.forwardRef<HTMLDivElement, ToastProps>(
           <div className="flex items-start">
             <div className="flex-shrink-0">
               {variant === "success" ? (
-                <CheckCircleIcon className="h-6 w-6" />
+                <CheckCircleIcon className="h-6 w-6" aria-hidden="true" />
               ) : variant === "warning" ? (
-                <ExclamationTriangleIcon className="h-6 w-6" />
+                <ExclamationTriangleIcon
+                  className="h-6 w-6"
+                  aria-hidden="true"
+                />
               ) : variant === "danger" ? (
-                <ExclamationCircleIcon className="h-6 w-6" />
+                <ExclamationCircleIcon className="h-6 w-6" aria-hidden="true" />
               ) : (
-                <ExclamationCircleIcon className="h-6 w-6" />
+                <QuestionMarkCircleIcon
+                  className="h-6 w-6"
+                  aria-hidden="true"
+                />
               )}
             </div>
             <div className="ml-3 w-0 flex-1 pt-0.5">
