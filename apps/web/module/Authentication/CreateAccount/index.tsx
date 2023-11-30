fbf0d5851afb1aebcaa88116f85284cb0b4c3bae
jazer <albalate.jazer@gmail.com>
2023-11-30 16:59:47 +0800

    feat(web): Created custom checkbox component

--- apps/web/module/Authentication/CreateAccount/index.tsx
+++ apps/web/module/Authentication/CreateAccount/index.tsx
@@ -3,7 +3,6 @@ import React from "react"
 import AuthContainer from "@/common/components/AuthContainer"
 import SignupInputs from "@/common/components/SignupInputs"
 import { useRouter } from "next/navigation"
-import { Checkbox } from "@/common/components/ui/Checkbox"
 
 const CreateAccount = () => {
   const router = useRouter()
@@ -13,7 +12,6 @@ const CreateAccount = () => {
       onBack={() => router.push("/")}
     >
       <SignupInputs />
-      <Checkbox checkboxId="IDKOTO" text="I don’t want to receive marketing messages from Explore Siargao" />
     </AuthContainer>
   )
 }
