8c529cafb87ed4394cda602b61ce5b24c38ecbb6
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-01 20:22:11 +0800

    fix the code smell

--- apps/web/module/Authentication/NewPassword/index.tsx
+++ apps/web/module/Authentication/NewPassword/index.tsx
@@ -86,13 +86,15 @@ const NewPassword = () => {
             </div>
             <Button className="w-full my-5" type="submit">
               {isPendingNewPassword ? (
-                  <div
-                    className="animate-spin inline-block w-[20px] h-[20px] border-[2px] border-current border-t-transparent text-white rounded-full"
-                    role={"status"}
-                    aria-label={"loading"}
-                  >
-                    <span className="sr-only">Loading...</span>
-                  </div>
+                  <div className="content px-4 md:px-7 lg:px-16 2xl:px-80 mt-28">
+                        <div
+                          className="m-4 animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
+                          role="status"
+                          aria-label="loading"
+                        >
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      </div>
                 ) : (
                   SUBMIT_BUTTON_TEXT
                 )
