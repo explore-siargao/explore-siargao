fafdb9661129101327e33b995fcb18529c4a31f6
ramil25 <ramil25@users.noreply.github.com>
2023-12-02 03:11:53 +0000

    Prettified Code!

--- apps/web/module/Authentication/NewPassword/index.tsx
+++ apps/web/module/Authentication/NewPassword/index.tsx
@@ -87,9 +87,7 @@ const NewPassword = () => {
             <Button className="w-full my-5" type="submit">
               {isPendingNewPassword ? (
                 <div className="content px-4 md:px-7 lg:px-16 2xl:px-80 mt-28">
-                  <div
-                    className="m-4 animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
-                  >
+                  <div className="m-4 animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full">
                     <span className="sr-only">Loading...</span>
                   </div>
                 </div>
