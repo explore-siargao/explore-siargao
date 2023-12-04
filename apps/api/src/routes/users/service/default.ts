86c2fce66b91f62286ac7ca8cb4b8b1c30605481
ramil25 <ramil25@users.noreply.github.com>
2023-12-04 06:57:56 +0000

    Prettified Code!

--- apps/api/src/routes/users/service/default.ts
+++ apps/api/src/routes/users/service/default.ts
@@ -8,11 +8,11 @@ export const getAllUsers = async (req: Request, res: Response) => {
     const prisma = new PrismaClient()
     const users = await prisma.user.findMany({
       include: {
-        personalInfo:{
-          include:{
-            emergrncyContacts:true
-          }
-        }
+        personalInfo: {
+          include: {
+            emergrncyContacts: true,
+          },
+        },
       },
     })
     if (users.length > 0) {
--- apps/api/src/routes/users/service/personalInfo.ts
+++ apps/api/src/routes/users/service/personalInfo.ts
@@ -1,6 +1,6 @@
 import { Response, Request } from 'express'
 import { PrismaClient } from '@prisma/client'
-import { REQUIRED_VALUE_EMPTY} from '@repo/constants'
+import { REQUIRED_VALUE_EMPTY } from '@repo/constants'
 export const getPersonalInfo = async (req: Request, res: Response) => {
   try {
     const prisma = new PrismaClient()
@@ -37,61 +37,59 @@ export const getPersonalInfo = async (req: Request, res: Response) => {
   }
 }
 
-export const addEmergencyContact = async (req:Request, res:Response)=>{
-    try{
+export const addEmergencyContact = async (req: Request, res: Response) => {
+  try {
     const prisma = new PrismaClient()
-    const {email, phoneNumber, name, relationship} = req.body
+    const { email, phoneNumber, name, relationship } = req.body
     const personalInfoId = Number(req.params.personalInfoId)
-    if(name && relationship && (email || phoneNumber)){
-    const getPersonalInfo = await prisma.personalInfo.findFirst({
-        where:{
-            id:personalInfoId,
-            deletedAt: null
-        }
-    })
-
-    if(getPersonalInfo){
-    const newEmergencyContact = await prisma.emergencyContacts.create({
-        data:{
-        peronalInfoId: personalInfoId,
-         email: email,
-         phoneNumber : phoneNumber,
-         name: name,
-         relationship: relationship  
-        }
-    })
-
-    res.json({
-        error: false,
-        items: newEmergencyContact,
-        itemCount: 1,
-        message: 'Emergency Contact Successfully Added',
+    if (name && relationship && (email || phoneNumber)) {
+      const getPersonalInfo = await prisma.personalInfo.findFirst({
+        where: {
+          id: personalInfoId,
+          deletedAt: null,
+        },
       })
-    }else{
-        res.json({
-            error: true,
-            items: getPersonalInfo,
-            itemCount: 0,
-            message:"No personal information data found",
-          })
-    }
 
-}else{
-  console.log(email)
-    res.json({
+      if (getPersonalInfo) {
+        const newEmergencyContact = await prisma.emergencyContacts.create({
+          data: {
+            peronalInfoId: personalInfoId,
+            email: email,
+            phoneNumber: phoneNumber,
+            name: name,
+            relationship: relationship,
+          },
+        })
+
+        res.json({
+          error: false,
+          items: newEmergencyContact,
+          itemCount: 1,
+          message: 'Emergency Contact Successfully Added',
+        })
+      } else {
+        res.json({
+          error: true,
+          items: getPersonalInfo,
+          itemCount: 0,
+          message: 'No personal information data found',
+        })
+      }
+    } else {
+      console.log(email)
+      res.json({
         error: true,
-        items: (name && relationship),
+        items: name && relationship,
         itemCount: 0,
         message: REQUIRED_VALUE_EMPTY,
       })
-}
-
-    }catch(err: any){
-        res.json({
-            error: true,
-            items: null,
-            itemCount: 0,
-            message: err.message,
-          }) 
     }
+  } catch (err: any) {
+    res.json({
+      error: true,
+      items: null,
+      itemCount: 0,
+      message: err.message,
+    })
+  }
 }
