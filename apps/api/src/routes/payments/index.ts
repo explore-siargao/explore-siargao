716c521a3309ab3630b6133853e92dce91884dbf
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-05 14:17:23 +0800

    feat(web): Creted api route that will use to add payment method

---
+++ apps/api/src/routes/payments/index.ts
@@ -0,0 +1,9 @@
+import isUserLoggedIn from '@/common/middleware/isLoggedIn'
+import express from 'express'
+import { addpaymentMethod } from './service/paymentMethod'
+
+const router = express.Router()
+router.post('/:userId/paymentMethod', isUserLoggedIn, addpaymentMethod)
+
+
+export default router
