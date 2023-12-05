03e3a1b7dc5ebd644e69dc805759f3948d1b5e1b
ramil25 <ramil25@users.noreply.github.com>
2023-12-05 07:57:26 +0000

    Prettified Code!

--- apps/api/src/routes/payments/index.ts
+++ apps/api/src/routes/payments/index.ts
@@ -1,9 +1,17 @@
 import isUserLoggedIn from '@/common/middleware/isLoggedIn'
 import express from 'express'
-import { addpaymentMethod, getPaymentMethods, removePaymentmethod } from './service/paymentMethod'
+import {
+  addpaymentMethod,
+  getPaymentMethods,
+  removePaymentmethod,
+} from './service/paymentMethod'
 
 const router = express.Router()
 router.post('/:userId/paymentMethod', isUserLoggedIn, addpaymentMethod)
 router.get('/:userId/paymentMethod', isUserLoggedIn, getPaymentMethods)
-router.delete('/:userId/paymentMethod/remove/:paymentMethodId', isUserLoggedIn, removePaymentmethod)
+router.delete(
+  '/:userId/paymentMethod/remove/:paymentMethodId',
+  isUserLoggedIn,
+  removePaymentmethod
+)
 export default router
