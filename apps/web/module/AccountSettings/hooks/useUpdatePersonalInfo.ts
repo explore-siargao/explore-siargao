8c2a1f67e3561bf2b1e3f3318ab27f4f9b57a8eb
ramil25 <ramil25@users.noreply.github.com>
2023-12-06 06:06:17 +0000

    Prettified Code!

--- apps/web/module/AccountSettings/hooks/useUpdatePersonalInfo.ts
+++ apps/web/module/AccountSettings/hooks/useUpdatePersonalInfo.ts
@@ -4,16 +4,22 @@ import { API_URL_USERS } from "@repo/constants"
 import { useMutation } from "@tanstack/react-query"
 import Cookies from "js-cookie"
 
-export async function updatePersonalInfo(userId: number | undefined, props: IPersonalInfo) {
+export async function updatePersonalInfo(
+  userId: number | undefined,
+  props: IPersonalInfo
+) {
   const token = Cookies.get("accessToken")
   const apiService = new ApiService()
-  return await apiService.patch(`${API_URL_USERS}/personal-info/${userId}`, props)
+  return await apiService.patch(
+    `${API_URL_USERS}/personal-info/${userId}`,
+    props
+  )
 }
 
-function useUpdatePersonalInfo(userId:number) {
-    const query = useMutation({
-      mutationFn: (props: IPersonalInfo) => updatePersonalInfo(userId,props),
-    })
-    return query
-  }
+function useUpdatePersonalInfo(userId: number) {
+  const query = useMutation({
+    mutationFn: (props: IPersonalInfo) => updatePersonalInfo(userId, props),
+  })
+  return query
+}
 export default useUpdatePersonalInfo
