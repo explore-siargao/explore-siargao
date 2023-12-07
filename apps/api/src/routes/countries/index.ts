6a3afa5b6ad8984eaedfa476eb1750a66273bdc7
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-07 15:55:07 +0800

    feat(web): Hook for getting country list done

---
+++ apps/api/src/routes/countries/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import { getCountries } from './service/country'
+
+
+const router = express.Router()
+
+// DEFAULT
+router.get('/', getCountries)
+
+
+export default router
