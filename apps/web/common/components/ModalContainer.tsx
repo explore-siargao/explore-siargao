3d41497cc706c0faceda0fbdfac7b10fb562b814
jazer <albalate.jazer@gmail.com>
2023-12-07 16:05:29 +0800

    refactor(web): Fix code smell

--- apps/web/common/components/ModalContainer.tsx
+++ apps/web/common/components/ModalContainer.tsx
@@ -7,38 +7,35 @@ type Props = {
   onClose?: () => void
   onBack?: () => void
 }
+
 const ModalContainer = ({ children, title, onClose, onBack }: Props) => {
   return (
-    <>
-      <div className="bg-white shadow rounded-2xl ">
-        <div className="flex border-b-gray-200 border-b p-4">
-          <div>
-            {onClose ? (
-              <XMarkIcon
-                className="h-6 w-6 cursor-pointer rounded-full hover:bg-gray-300/30"
-                onClick={() => onClose()}
-              />
-            ) : null}
-            {onBack ? (
-              <ArrowLeftIcon
-                className="h-6 w-6 cursor-pointer rounded-full hover:bg-gray-300/30"
-                onClick={() => onBack()}
-              />
-            ) : null}
-          </div>
-          <div className="flex-1">
-            <h1
-              className={`w-full text-center place-self-center font-semibold`}
-            >
-              <span className={`${onClose || onBack ? "-ml-4" : ""}`}>
-                {title}
-              </span>
-            </h1>
-          </div>
+    <div className="bg-white shadow rounded-2xl ">
+      <div className="flex border-b-gray-200 border-b p-4">
+        <div>
+          {onClose ? (
+            <XMarkIcon
+              className="h-6 w-6 cursor-pointer rounded-full hover:bg-gray-300/30"
+              onClick={() => onClose()}
+            />
+          ) : null}
+          {onBack ? (
+            <ArrowLeftIcon
+              className="h-6 w-6 cursor-pointer rounded-full hover:bg-gray-300/30"
+              onClick={() => onBack()}
+            />
+          ) : null}
         </div>
-        <div>{children}</div>
+        <div className="flex-1">
+          <h1 className={`w-full text-center place-self-center font-semibold`}>
+            <span className={`${onClose || onBack ? "-ml-4" : ""}`}>
+              {title}
+            </span>
+          </h1>
+        </div>
       </div>
-    </>
+      <div>{children}</div>
+    </div>
   )
 }
 
