9fc9560af4a7bbbc823f6994e81bf8971a17c11e
jazer <albalate.jazer@gmail.com>
2023-12-11 16:45:12 +0800

    refactor(web): Fix the commented in header

--- apps/web/module/LandingPage/components/Header.tsx
+++ apps/web/module/LandingPage/components/Header.tsx
@@ -9,11 +9,11 @@ import { APP_NAME } from "@repo/constants"
 function Header() {
   return (
     <header className="absolute inset-x-0 top-0 z-50 bg-white border-y-gray-200/50 border">
-      {/* <div className="w-full text-center py-2 bg-gray-50">
+      <div className="w-full text-center py-2 bg-gray-50 sr-only md:not-sr-only">
         <p className="underline">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-      </div> */}
+      </div>
       <nav
         className="flex items-center justify-between p-2 mx-2 my-2 md:mx-10 lg:px-8"
         aria-label="Global"
