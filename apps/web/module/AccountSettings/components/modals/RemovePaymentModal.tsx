a3898ced99a973338e4fe6ef91cc398598014a5d
jazer <albalate.jazer@gmail.com>
2023-12-12 14:05:54 +0800

    refactor(web): Fixed code smell

--- apps/web/module/AccountSettings/components/modals/RemovePaymentModal.tsx
+++ apps/web/module/AccountSettings/components/modals/RemovePaymentModal.tsx
@@ -8,15 +8,18 @@ interface PaymentModalProps {
   onClose: () => void
 }
 
-const RemovePaymentModal = ({ isOpen, onClose }: PaymentModalProps) => {
+const RemovePaymentModal = ({
+  isOpen: openModal,
+  onClose: closeModal,
+}: PaymentModalProps) => {
   const cancelButtonRef = useRef(null)
   return (
-    <Transition.Root show={isOpen} as={Fragment}>
+    <Transition.Root show={openModal} as={Fragment}>
       <Dialog
         as="div"
         className="relative z-50"
         initialFocus={cancelButtonRef}
-        onClose={onClose}
+        onClose={closeModal}
       >
         <Transition.Child
           as={Fragment}
@@ -42,14 +45,17 @@ const RemovePaymentModal = ({ isOpen, onClose }: PaymentModalProps) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:w-full sm:max-w-lg ">
-                <ModalContainer title="Remove payment method" onClose={onClose}>
+                <ModalContainer
+                  title="Remove payment method"
+                  onClose={closeModal}
+                >
                   <div className="p-6">
                     <p className="text-text-400 font-light">
                       Are you sure you want to remove this payment method?
                     </p>
                   </div>
                   <div className="flex justify-between px-4 py-2 border-t-gray-200 border-t">
-                    <Button variant={"ghost"} onClick={onClose}>
+                    <Button variant={"ghost"} onClick={closeModal}>
                       Cancel
                     </Button>
                     <Button>Remove</Button>
