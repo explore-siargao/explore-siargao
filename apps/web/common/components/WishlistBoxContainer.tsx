9bcea96c9e165b271a936015a1d7a8794958ee0e
jazer <albalate.jazer@gmail.com>
2023-12-13 09:04:40 +0800

    refactor(web): Fix code smell

--- apps/web/common/components/WishlistBoxContainer.tsx
+++ apps/web/common/components/WishlistBoxContainer.tsx
@@ -9,25 +9,23 @@ type Props = {
 }
 const WishlistBoxContainer = ({ title, text, photo }: Props) => {
   return (
-    <>
-      <div>
-        <div className="h-[150px] w-[150px] md:h-[290px] md:w-[290px] rounded-2xl p-1 border border-text-100 shadow-md">
-          <Image
-            src={photo}
-            width={300}
-            height={300}
-            alt={photo}
-            className="box-border h-full w-full rounded-xl shadow-md"
-          />
-        </div>
-        <div className="mt-2">
-          <Title size={"ContentTitle"} className="text-text-500">
-            {title}
-          </Title>
-          <p className="text-text-300">{text}</p>
-        </div>
+    <div>
+      <div className="h-[150px] w-[150px] md:h-[290px] md:w-[290px] rounded-2xl p-1 border border-text-100 shadow-md">
+        <Image
+          src={photo}
+          width={300}
+          height={300}
+          alt={photo}
+          className="box-border h-full w-full rounded-xl shadow-md"
+        />
       </div>
-    </>
+      <div className="mt-2">
+        <Title size={"ContentTitle"} className="text-text-500">
+          {title}
+        </Title>
+        <p className="text-text-300">{text}</p>
+      </div>
+    </div>
   )
 }
 
