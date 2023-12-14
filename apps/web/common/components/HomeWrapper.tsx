41cf29ad302365465af04a89aa7edca6f2731d75
albalatejazer <albalatejazer@users.noreply.github.com>
2023-12-14 07:02:58 +0000

    Prettified Code!

--- apps/web/common/components/HomeWrapper.tsx
+++ apps/web/common/components/HomeWrapper.tsx
@@ -4,11 +4,7 @@ type Props = {
   children: React.ReactNode
 }
 const HomeWrapper = ({ children }: Props) => {
-  return (
-    <div className="my-40 justify-center mx-5 md:mx-20">
-      {children}
-    </div>
-  )
+  return <div className="my-40 justify-center mx-5 md:mx-20">{children}</div>
 }
 
 export default HomeWrapper
