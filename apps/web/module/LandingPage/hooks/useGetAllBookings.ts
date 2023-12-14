42075ce7be49612db7e884511351de056666564f
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-14 16:52:33 +0800

    feat(web): Created custom hooks for getting all bookings

---
+++ apps/web/module/LandingPage/hooks/useGetAllBookings.ts
@@ -0,0 +1,18 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_BOOKINGS} from "@repo/constants"
+import { useQuery } from "@tanstack/react-query"
+
+export async function getAllBookings() {
+  const apiService = new ApiService()
+  return await apiService.get(`${API_URL_BOOKINGS}`)
+}
+
+function useGetAllBookings() {
+  const query = useQuery({
+    queryKey: ["bookings"],
+    queryFn: () => getAllBookings(),
+    refetchOnWindowFocus: false,
+  })
+  return query
+}
+export default useGetAllBookings
