91b4e12cdc2e2bb255dee556654e09c48ebdd668
ramil25 <ramil25@users.noreply.github.com>
2023-12-15 03:13:07 +0000

    Prettified Code!

--- apps/web/common/components/ui/Spinner.tsx
+++ apps/web/common/components/ui/Spinner.tsx
@@ -4,12 +4,12 @@ import { VariantProps, cva } from "class-variance-authority"
 const spinnerVariants = cva("font-bold", {
   variants: {
     variant: {
-      default:"text-primary-300",
-      primary:"text-primary-600",
-      danger:"text-error-500",
-      warning:"text-warning-500",
-      success:"text-success-700",
-      secondary:"text-secondary-500"
+      default: "text-primary-300",
+      primary: "text-primary-600",
+      danger: "text-error-500",
+      warning: "text-warning-500",
+      success: "text-success-700",
+      secondary: "text-secondary-500",
     },
     size: {
       sm: "w-4 h-4",
@@ -17,20 +17,25 @@ const spinnerVariants = cva("font-bold", {
       lg: "w-12 h-12",
     },
   },
-  defaultVariants: { 
-    variant:"default",
+  defaultVariants: {
+    variant: "default",
     size: "sm",
   },
 })
 export interface SpinnerProps
   extends React.DetailsHTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof spinnerVariants> {
-}
+    VariantProps<typeof spinnerVariants> {}
 const Spinner = React.forwardRef<HTMLDivElement, SpinnerProps>(
-  ({variant, className, size }) => {
-    return <div className={`animate-spin border-2 border-current border-t-transparent rounded-full ${cn(spinnerVariants({variant, size, className }))}`}>
-      <span className="sr-only">Loading...</span>
-    </div>
+  ({ variant, className, size }) => {
+    return (
+      <div
+        className={`animate-spin border-2 border-current border-t-transparent rounded-full ${cn(
+          spinnerVariants({ variant, size, className })
+        )}`}
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+    )
   }
 )
 Spinner.displayName = "Spinner"
