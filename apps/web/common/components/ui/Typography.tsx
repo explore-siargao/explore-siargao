77b2e8954c34287a6b1f637399a18f62c211ba05
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-15 13:30:11 +0800

    feat(web): Added reusable component typography

---
+++ apps/web/common/components/ui/Typography.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { cn } from "@/common/helpers/cn"
+import { VariantProps, cva } from "class-variance-authority"
+const typographyVariants = cva("font-bold", {
+  variants: {
+    variant: {
+      p: "text-base",
+      h1: "text-3xl font-bold",
+      h2: "text-2xl font-bold",
+      h3: "text-xl font-bold",
+      h4: "text-lg font-bold",
+      h5: "text-sm font-bold",
+      h6: "text-xs font-bold",
+    },
+    fontWeight:{
+        normal:"font-normal",
+        bold:"font-bold",
+        thin:"font-thin",
+        semiBold:"font-semibold",
+    }
+  },
+  defaultVariants: {
+    variant: "p",
+    fontWeight:"normal"
+  },
+})
+export interface TypographyProps
+  extends React.DetailsHTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof typographyVariants> {
+        children: string
+    }
+
+const Typography = React.forwardRef<HTMLDivElement, TypographyProps>(
+  ({ variant, fontWeight, className, children }) => {
+    return (
+      <p className={cn(typographyVariants({ variant, fontWeight, className}))}>{children}</p>
+    )
+  }
+)
+Typography.displayName = "Typography"
+
+export { Typography, typographyVariants }
