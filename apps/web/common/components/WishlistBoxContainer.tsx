c51b779ab50910d78318d29c20c1c5d1e55d92bc
jazer <albalate.jazer@gmail.com>
2023-12-18 14:56:05 +0800

    refactor(web): Fixed the code smell that sonar cloud detected

--- apps/web/common/components/WishlistBoxContainer.tsx
+++ apps/web/common/components/WishlistBoxContainer.tsx
@@ -16,18 +16,20 @@ const WishlistBoxContainer = ({ title, text, photo }: Props) => {
 
   return (
     <>
-      <div>
+      <div className="flex flex-col">
         <div
           className="h-[150px] w-[150px] md:h-72 md:w-auto 2xl:h-72 2xl:w-auto rounded-3xl relative select-none border border-text-100"
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
+          role="button"
         >
           {isHovered && (
-            <XMarkIcon
-              role="button"
-              className="absolute top-4 left-4 h-8 w-8 text-text-400 bg-primary-100 rounded-full p-1 hover:bg-white hover:shadow-lg transition hover:scale-105"
-              onClick={() => setRemoveModal(true)}
-            />
+            <div onClick={() => setRemoveModal(true)}>
+              <XMarkIcon
+                role="button"
+                className="absolute top-4 left-4 h-8 w-8 text-text-400 bg-primary-100 rounded-full p-1 hover:bg-white hover:shadow-lg transition hover:scale-105"
+              />
+            </div>
           )}
           <Image
             src={photo}
