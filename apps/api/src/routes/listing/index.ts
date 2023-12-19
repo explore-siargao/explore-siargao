a929feebfb5f7a5318f81952f9fe838655efa849
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-19 10:41:32 +0800

    feat(api): Created api endpoints and routes for adding new listing

--- apps/api/src/routes/listing/index.ts
+++ apps/api/src/routes/listing/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllListing, getListing } from './service/default'
+import { addListing, getAllListing, getListing } from './service/listing'
 import isCsrfTokenValid from '@/common/middleware/auth/isCsrfTokenValid'
 import isOriginValid from '@/common/middleware/auth/isOriginValid'
 import { getWishGroupsByUser } from './service/wishGroup'
@@ -8,8 +8,9 @@ import isUserLoggedIn from '@/common/middleware/auth/isUserLoggedIn'
 const router = express.Router()
 
 // DEFAULT
-router.get('/', isCsrfTokenValid, isOriginValid, getAllListing)
+router.get('/',isCsrfTokenValid, isOriginValid, getAllListing)
 router.get('/:id', isCsrfTokenValid, isOriginValid, getListing)
+router.post('/:hostId',isCsrfTokenValid, isOriginValid, isUserLoggedIn, addListing)
 router.get(
   '/:userId/wish-group',
   isOriginValid,
