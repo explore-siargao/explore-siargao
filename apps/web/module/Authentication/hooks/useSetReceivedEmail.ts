2235cb88f0ee10a64618f405b36fa291929b6fe9
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-12-21 14:03:16 +0800

    feat(web): Created custom hook for setting user canreceivedEmail

---
+++ apps/web/module/Authentication/hooks/useSetReceivedEmail.ts
@@ -0,0 +1,16 @@
+import { API_URL_USERS } from "@repo/constants"
+import { useMutation } from "@tanstack/react-query"
+import { ApiService } from "@/common/service/api"
+
+export async function setReceivedEmail(userId:number | undefined) {
+  const apiService = new ApiService()
+  return await apiService.patch(`${API_URL_USERS}/${userId}`)
+}
+function useSetReceivedEmail(userId:number) {
+  const query = useMutation({
+    mutationFn: () => setReceivedEmail(userId),
+  })
+  return query
+}
+
+export default useSetReceivedEmail
