fd44bac88ff2915daff065f5d13e393a247f2567
ramil25 <ramil25@users.noreply.github.com>
2023-12-21 06:05:36 +0000

    Prettified Code!

--- apps/web/module/Authentication/hooks/useSetReceivedEmail.ts
+++ apps/web/module/Authentication/hooks/useSetReceivedEmail.ts
@@ -2,11 +2,11 @@ import { API_URL_USERS } from "@repo/constants"
 import { useMutation } from "@tanstack/react-query"
 import { ApiService } from "@/common/service/api"
 
-export async function setReceivedEmail(userId:number | undefined) {
+export async function setReceivedEmail(userId: number | undefined) {
   const apiService = new ApiService()
   return await apiService.patch(`${API_URL_USERS}/${userId}`)
 }
-function useSetReceivedEmail(userId:number) {
+function useSetReceivedEmail(userId: number) {
   const query = useMutation({
     mutationFn: () => setReceivedEmail(userId),
   })
