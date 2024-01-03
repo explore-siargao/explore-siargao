04c08a01f0187c9de1d1e9315adb0a98dd6d5992
ramil25 <ramil25@users.noreply.github.com>
2024-01-03 08:28:00 +0000

    Prettified Code!

--- packages/contract/src/WishGroup/zod.ts
+++ packages/contract/src/WishGroup/zod.ts
@@ -2,12 +2,12 @@ import { z } from "zod"
 export const Z_WishGroup = z.object({
   id: z.number().optional(),
   userId: z.number().optional(),
- title: z.string().optional(),
- newTitle: z.string().optional(),
- oldTitle: z.string().optional(),
- listingId: z.number().optional(),
- note:z.string().nullable().optional(),
- createdAt: z.date().optional(),
+  title: z.string().optional(),
+  newTitle: z.string().optional(),
+  oldTitle: z.string().optional(),
+  listingId: z.number().optional(),
+  note: z.string().nullable().optional(),
+  createdAt: z.date().optional(),
   updatedAt: z.date().nullable().optional(),
   deletedAt: z.date().nullable().optional(),
 })
