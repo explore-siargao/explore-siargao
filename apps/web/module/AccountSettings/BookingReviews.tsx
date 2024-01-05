f506e511fb5f3667f13bfe82efcfaf7f683fc637
jazer <albalate.jazer@gmail.com>
2024-01-05 16:21:58 +0800

    feat(web):Created booking review page

--- apps/web/module/AccountSettings/BookingReviews.tsx
+++ apps/web/module/AccountSettings/BookingReviews.tsx
@@ -1,328 +1,46 @@
+"use client"
 import AccountSettingWrapper from "@/common/components/AccountSettingWrapper"
-import { Button } from "@/common/components/ui/Button"
-import { Typography } from "@/common/components/ui/Typography"
-import {
-  BeakerIcon,
-  ChatBubbleBottomCenterIcon,
-  CheckCircleIcon,
-  KeyIcon,
-  MapIcon,
-  TagIcon,
-} from "@heroicons/react/24/outline"
-import Image from "next/image"
 import React from "react"
+import BookingReviewItem from "./components/BookingReviewItem"
 
-const RatingBar: React.FC<{ rating: number }> = ({ rating }) => (
-  <div className="w-full bg-text-100 rounded h-1.5 me-2">
-    <div
-      className="bg-text-600 h-1.5 rounded "
-      style={{ width: `${rating * 10}%` }}
-    ></div>
-  </div>
-)
+const bookingReviewsDummy = [
+  {
+    id: 1,
+    joinedDate: "Joined on August 2014",
+    name: "Explore Siargao",
+    pic: "http://localhost:3000/5.jpg",
+    reviewMessage:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem omnis similique illum id quo soluta atque tenetur necessitatibus reprehenderit perspiciatis, dolores, aliquid voluptate aut maxime perferendis provident distinctio nulla magni alias dolore facilis? Accusantium, sit. Quo neque numquam itaque minus libero sapiente eum iste odio eius soluta ulla soluta ulla hello pmi nas an asd ! asda",
+    location: "United Kingdom",
+    reviewedTime: "March 3, 2017",
+  },
+  {
+    id: 2,
+    joinedDate: "Joined on August 2014",
+    name: "Explore Siargao",
+    pic: "http://localhost:3000/5.jpg",
+    location: "United Kingdom",
+    reviewMessage:
+      "Laborum nisi consectetur esse irure consequat nulla nulla eu fugiat duis incididunt quis laborum. Nulla ea adipisicing ex et occaecat commodo sint ea do officia irure. Aliquip do incididunt ut sunt. Aliquip velit non consequat velit sit minim laborum voluptate sunt ex excepteur ad deserunt.",
+    reviewedTime: "March 3, 2017",
+  },
+]
 
 const BookingReviews = () => {
   return (
-    <AccountSettingWrapper>
-      <div className="divide-y divide-text-100">
-        <article className="pt-10 hover:bg-primary-50 p-5">
-          <div className="flex justify-between">
-            <div className="flex items-center mb-4">
-              <Image
-                className="w-10 h-10 me-4 rounded-full"
-                src="http://localhost:3000/logo-single.png"
-                alt=""
-                width={300}
-                height={300}
-              />
-              <div className="font-medium ">
-                <p>
-                  Explore Siargao{" "}
-                  <time
-                    dateTime="2014-08-16 19:00"
-                    className="block text-sm text-gray-500 d"
-                  >
-                    Joined on August 2014
-                  </time>
-                </p>
-              </div>
-            </div>
-            <Button variant={"primary"}>View listing</Button>
-          </div>
-          <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500 "
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <h3 className="ms-2 text-sm font-semibold text-gray-900 ">
-              Thinking to buy another one!
-            </h3>
-          </div>
-          <footer className="mb-5 text-sm text-gray-500  ">
-            <p>
-              Reviewed in the United Kingdom on{" "}
-              {/* <time datetime="2017-03-03 19:00">March 3, 2017</time> */}
-            </p>
-          </footer>
-          <p className="mb-2 text-gray-500  ">
-            This is my third Invicta Pro Diver. They are just fantastic value
-            for money. This one arrived yesterday and the first thing I did was
-            set the time, popped on an identical strap from another Invicta and
-            went in the shower with it to test the waterproofing.... No
-            problems.
-          </p>
-          <p className="mb-3 text-gray-500  ">
-            It is obviously not the same build quality as those very expensive
-            watches. But that is like comparing a Citroën to a Ferrari. This
-            watch was well under £100! An absolute bargain.
-          </p>
-          <a
-            href="#"
-            className="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Read more
-          </a>
-        </article>
-        <article className="pt-10 hover:bg-primary-50 p-5">
-          <div className="flex justify-between">
-            <div className="flex items-center mb-4">
-              <Image
-                className="w-10 h-10 me-4 rounded-full"
-                src="http://localhost:3000/logo-single.png"
-                alt=""
-                width={300}
-                height={300}
-              />
-              <div className="font-medium ">
-                <p>
-                  Explore Siargao{" "}
-                  <time
-                    dateTime="2014-08-16 19:00"
-                    className="block text-sm text-gray-500 d"
-                  >
-                    Joined on August 2014
-                  </time>
-                </p>
-              </div>
-            </div>
-            <Button variant={"primary"}>View listing</Button>
-          </div>
-          <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500 "
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <h3 className="ms-2 text-sm font-semibold text-gray-900 ">
-              Thinking to buy another one!
-            </h3>
-          </div>
-          <footer className="mb-5 text-sm text-gray-500  ">
-            <p>
-              Reviewed in the United Kingdom on{" "}
-              {/* <time datetime="2017-03-03 19:00">March 3, 2017</time> */}
-            </p>
-          </footer>
-          <p className="mb-2 text-gray-500  ">
-            This is my third Invicta Pro Diver. They are just fantastic value
-            for money. This one arrived yesterday and the first thing I did was
-            set the time, popped on an identical strap from another Invicta and
-            went in the shower with it to test the waterproofing.... No
-            problems.
-          </p>
-          <p className="mb-3 text-gray-500  ">
-            It is obviously not the same build quality as those very expensive
-            watches. But that is like comparing a Citroën to a Ferrari. This
-            watch was well under £100! An absolute bargain.
-          </p>
-          <a
-            href="#"
-            className="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Read more
-          </a>
-        </article>
-        <article className="pt-10 hover:bg-primary-50 p-5">
-          <div className="flex justify-between">
-            <div className="flex items-center mb-4">
-              <Image
-                className="w-10 h-10 me-4 rounded-full"
-                src="http://localhost:3000/logo-single.png"
-                alt=""
-                width={300}
-                height={300}
-              />
-              <div className="font-medium ">
-                <p>
-                  Explore Siargao{" "}
-                  <time
-                    dateTime="2014-08-16 19:00"
-                    className="block text-sm text-gray-500 d"
-                  >
-                    Joined on August 2014
-                  </time>
-                </p>
-              </div>
-            </div>
-            <Button variant={"primary"}>View listing</Button>
-          </div>
-          <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <svg
-              className="w-4 h-4 text-text-500 "
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-            </svg>
-            <h3 className="ms-2 text-sm font-semibold text-gray-900 ">
-              Thinking to buy another one!
-            </h3>
-          </div>
-          <footer className="mb-5 text-sm text-gray-500  ">
-            <p>
-              Reviewed in the United Kingdom on{" "}
-              {/* <time datetime="2017-03-03 19:00">March 3, 2017</time> */}
-            </p>
-          </footer>
-          <p className="mb-2 text-gray-500  ">
-            This is my third Invicta Pro Diver. They are just fantastic value
-            for money. This one arrived yesterday and the first thing I did was
-            set the time, popped on an identical strap from another Invicta and
-            went in the shower with it to test the waterproofing.... No
-            problems.
-          </p>
-          <p className="mb-3 text-gray-500  ">
-            It is obviously not the same build quality as those very expensive
-            watches. But that is like comparing a Citroën to a Ferrari. This
-            watch was well under £100! An absolute bargain.
-          </p>
-          <a
-            href="#"
-            className="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-          >
-            Read more
-          </a>
-        </article>
-      </div>
+    <AccountSettingWrapper className="mt-20 md:mt-32 lg:mt-36 gap-y-4">
+      {bookingReviewsDummy.map((item) => (
+        <BookingReviewItem
+          id={item.id}
+          joinedDate={item.joinedDate}
+          location={item.location}
+          name={item.name}
+          pic={item.pic}
+          reviewMessage={item.reviewMessage}
+          reviewedTime={item.reviewedTime}
+          key={item.id}
+        />
+      ))}
     </AccountSettingWrapper>
   )
 }
