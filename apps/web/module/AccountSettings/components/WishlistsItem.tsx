df862076a9ebdf2814df612d308a791267dc2e4b
ramil25 <ramil25@users.noreply.github.com>
2024-01-08 06:25:57 +0000

    Prettified Code!

--- apps/web/module/AccountSettings/components/WishlistsItem.tsx
+++ apps/web/module/AccountSettings/components/WishlistsItem.tsx
@@ -6,18 +6,18 @@ import React from "react"
 
 const WishlistsItem = () => {
   return (
-      <WidthWrapper width={"full"} className="pt-24 lg:pt-28 overflow-y-hidden">
-        <div className="flex gap-2">
-          <div className="relative px-5">
-            <WishlistsItemContainer />
-          </div>
-          <div className="relative w-full hidden lg:block">
-            <div>
-              <WorldMap />
-            </div>
+    <WidthWrapper width={"full"} className="pt-24 lg:pt-28 overflow-y-hidden">
+      <div className="flex gap-2">
+        <div className="relative px-5">
+          <WishlistsItemContainer />
+        </div>
+        <div className="relative w-full hidden lg:block">
+          <div>
+            <WorldMap />
           </div>
         </div>
-      </WidthWrapper>
+      </div>
+    </WidthWrapper>
   )
 }
 
