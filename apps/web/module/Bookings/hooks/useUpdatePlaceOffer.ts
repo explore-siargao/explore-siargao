18cf09167021a91f9b9b9bf28dfb0fd04c64e6e6
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-18 16:49:26 +0800

    feat(web): Added custom hooks for place offers

---
+++ apps/web/module/Bookings/hooks/useUpdatePlaceOffer.ts
@@ -0,0 +1,24 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_BOOKINGS } from "@repo/constants"
+import { T_PlaceOffers } from "@repo/contract"
+import { useMutation } from "@tanstack/react-query"
+
+export async function updatePlaceOffer(
+  userId: number | null,
+  props: T_PlaceOffers
+) {
+  const apiService = new ApiService()
+  return await apiService.patch(
+    `${API_URL_BOOKINGS}/${userId}/place-offers/${props.id}`,
+    props
+  )
+}
+
+function useUpdatePlaceOffer(userId: number | null) {
+  const query = useMutation({
+    mutationFn: (props: T_PlaceOffers) =>
+    updatePlaceOffer(userId, props),
+  })
+  return query
+}
+export default useUpdatePlaceOffer
