d1e2d8c29e303b0a503763b08a87a79068c99c39
ramil25 <ramil25@users.noreply.github.com>
2024-01-18 07:46:32 +0000

    Prettified Code!

--- apps/web/module/Bookings/hooks/useAddReview.ts
+++ apps/web/module/Bookings/hooks/useAddReview.ts
@@ -3,10 +3,7 @@ import { API_URL_BOOKINGS } from "@repo/constants"
 import { T_Review } from "@repo/contract"
 import { useMutation } from "@tanstack/react-query"
 
-export async function addReview(
-  userId: number | undefined,
-  props: T_Review
-) {
+export async function addReview(userId: number | undefined, props: T_Review) {
   const apiService = new ApiService()
   return await apiService.post(
     `${API_URL_BOOKINGS}/${userId}/reviews/post`,
