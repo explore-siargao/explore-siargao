a1de2e3314128c21a6fe3682ec7cab5399c34880
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-18 14:10:16 +0800

    feat(api): Upload image of government Id

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import cookies from 'cookie-parser'
 import { port, origins } from '@/common/config'
+import fileupload from 'express-fileupload';
 import routes from '@/routes'
-import fileUpload from "express-fileupload"
+
 const es = express()
 es.disable('x-powered-by')
 es.use(cookies())
 es.use(express.json())
-es.use(fileUpload())
+es.use(fileupload());
 es.use(
   cors({
     origin: origins,
