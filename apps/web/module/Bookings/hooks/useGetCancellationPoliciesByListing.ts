9b6f8490d00c062ae04e38dc8cddc6a96af444a1
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-22 08:53:42 +0800

    feat(web): Added custom hooks for cancellation policy

---
+++ apps/web/module/Bookings/hooks/useGetCancellationPoliciesByListing.ts
@@ -0,0 +1,19 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_BOOKINGS } from "@repo/constants"
+import { useQuery } from "@tanstack/react-query"
+
+export async function getCancellationPoliciesByListingId(listingId: number | undefined) {
+  const apiService = new ApiService()
+  return await apiService.get(`${API_URL_BOOKINGS}/cancellation-policies/${listingId}`)
+}
+
+function useGetCancellationPoliciesByListing(listingId: number | undefined) {
+  const query = useQuery({
+    queryKey: ["cancellation-policies", listingId],
+    queryFn: () => getCancellationPoliciesByListingId(listingId),
+    refetchOnWindowFocus: false,
+    enabled: !!listingId,
+  })
+  return query
+}
+export default useGetCancellationPoliciesByListing
---
+++ apps/web/module/Bookings/hooks/useGetCancellationPolicy.ts
@@ -0,0 +1,19 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_BOOKINGS } from "@repo/constants"
+import { useQuery } from "@tanstack/react-query"
+
+export async function getCancellationPolicyById(id: number | undefined) {
+  const apiService = new ApiService()
+  return await apiService.get(`${API_URL_BOOKINGS}/cancellation-policies/id/${id}`)
+}
+
+function useGetCancellationPolicy(id: number | undefined) {
+  const query = useQuery({
+    queryKey: ["cancellation-policies", id],
+    queryFn: () => getCancellationPolicyById(id),
+    refetchOnWindowFocus: false,
+    enabled: !!id,
+  })
+  return query
+}
+export default useGetCancellationPolicy
