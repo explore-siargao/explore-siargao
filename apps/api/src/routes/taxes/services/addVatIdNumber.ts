06ea88b3bd076b6a784a77eed04fdcb8cbba79ab
alkejah <alkejah@users.noreply.github.com>
2024-01-26 06:38:04 +0000

    Prettified Code!

--- apps/api/src/routes/taxes/services/addVatIdNumber.ts
+++ apps/api/src/routes/taxes/services/addVatIdNumber.ts
@@ -8,72 +8,74 @@ const prisma = new PrismaClient()
 const response = new ResponseService()
 
 export const addTaxes = async (req: Request, res: Response) => {
-    const { 
-        countryRegion, 
-        vatId, 
-        nameOnRegistration, 
-        addressLine1,
-        addressLine2,
-        city,
-        provinceRegion,
-        zipPostalCode } = req.body
-    const userId = Number(req.params.userId)
-    try {
-      const getUser = await prisma.user.findUnique({
-        where: {
-          id: userId,
-        },
-      })
-      if (getUser) {
-        if ( 
-            countryRegion &&
-            vatId &&
-            nameOnRegistration &&
-            addressLine1 &&
-            addressLine2 &&
-            city &&
-            provinceRegion &&
-            zipPostalCode ) {
-          const inputIsValid = Z_Taxes.safeParse(req.body)
-          if (inputIsValid.success) {
-            const newTaxes = await prisma.taxes.create({
-              data: {
-                countryRegion: countryRegion,
-                vatId: vatId,
-                nameOnRegistration: nameOnRegistration,
-                addressLine1: addressLine1,
-                addressLine2: addressLine2,
-                city: city,
-                provinceRegion: provinceRegion,
-                zipPostalCode: zipPostalCode,
-              },
-            })
-            res.json({
-              success: true,
-              data: {
-                item: newTaxes,
-                allItemCount: 1,
-                message: 'New Tax successfully created',
-              },
-            });
-          } else {
-            res.json(
-              response.error({
-                message: JSON.parse(inputIsValid.error.message),
-              })
-            )
-          }
+  const {
+    countryRegion,
+    vatId,
+    nameOnRegistration,
+    addressLine1,
+    addressLine2,
+    city,
+    provinceRegion,
+    zipPostalCode,
+  } = req.body
+  const userId = Number(req.params.userId)
+  try {
+    const getUser = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+    if (getUser) {
+      if (
+        countryRegion &&
+        vatId &&
+        nameOnRegistration &&
+        addressLine1 &&
+        addressLine2 &&
+        city &&
+        provinceRegion &&
+        zipPostalCode
+      ) {
+        const inputIsValid = Z_Taxes.safeParse(req.body)
+        if (inputIsValid.success) {
+          const newTaxes = await prisma.taxes.create({
+            data: {
+              countryRegion: countryRegion,
+              vatId: vatId,
+              nameOnRegistration: nameOnRegistration,
+              addressLine1: addressLine1,
+              addressLine2: addressLine2,
+              city: city,
+              provinceRegion: provinceRegion,
+              zipPostalCode: zipPostalCode,
+            },
+          })
+          res.json({
+            success: true,
+            data: {
+              item: newTaxes,
+              allItemCount: 1,
+              message: 'New Tax successfully created',
+            },
+          })
         } else {
           res.json(
             response.error({
-              message: REQUIRED_VALUE_EMPTY,
+              message: JSON.parse(inputIsValid.error.message),
             })
           )
         }
       } else {
-        res.json(response.error({ message: USER_NOT_EXIST }))
+        res.json(
+          response.error({
+            message: REQUIRED_VALUE_EMPTY,
+          })
+        )
       }
-    } catch (err: any) {
-      res.json(response.error({ message: err.message }))
+    } else {
+      res.json(response.error({ message: USER_NOT_EXIST }))
     }
+  } catch (err: any) {
+    res.json(response.error({ message: err.message }))
   }
+}
