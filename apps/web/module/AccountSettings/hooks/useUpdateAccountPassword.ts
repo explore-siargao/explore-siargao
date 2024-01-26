b7a3ca97ece23abbeac3515c001dd9d75265c73f
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-26 12:21:05 +0800

    task(web): Added custom hook for updating account password

---
+++ apps/web/module/AccountSettings/hooks/useUpdateAccountPassword.ts
@@ -0,0 +1,23 @@
+import { ApiService } from "@/common/service/api"
+import { ICoupon } from "@/common/types/global"
+import { API_URL_PAYMENTS, API_URL_USERS } from "@repo/constants"
+import { useMutation } from "@tanstack/react-query"
+
+interface IChangePassword{
+    currentPassword: string
+    newPassword: string,
+    confirmPassword: string
+}
+
+export async function updateUserPassword(userId: number | null, props: IChangePassword) {
+  const apiService = new ApiService()
+  return await apiService.patch(`${API_URL_USERS}/change-password/${userId}`, props)
+}
+
+function useUpdateAccountPassword(userId: number | null) {
+  const query = useMutation({
+    mutationFn: (props: IChangePassword) => updateUserPassword(userId, props),
+  })
+  return query
+}
+export default useUpdateAccountPassword
