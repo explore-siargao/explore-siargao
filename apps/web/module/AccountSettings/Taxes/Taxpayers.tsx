666253d4602de3ffcaa559c80a73a4e361832217
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-26 12:52:16 +0800

    feat: Added Taxes page for account settings

---
+++ apps/web/module/AccountSettings/Taxes/Taxpayers.tsx
@@ -0,0 +1,157 @@
+import { Button } from "@/common/components/ui/Button"
+import { Input } from "@/common/components/ui/Input"
+import { IPersonalInfo } from "@/common/types/global"
+import React, { useState } from "react"
+import useUpdatePersonalInfo from "../hooks/useUpdatePersonalInfo"
+import { useQueryClient } from "@tanstack/react-query"
+import toast from "react-hot-toast"
+import { useForm } from "react-hook-form"
+import { Typography } from "@/common/components/ui/Typography"
+type PersonalInfoProps = {
+  isButtonClicked: boolean
+  contentId: string
+}
+
+const Taxpayers = ({ firstName, lastName, userId }: IPersonalInfo) => {
+  const [contentState, setContentState] = useState<PersonalInfoProps>({
+    isButtonClicked: false,
+    contentId: "",
+  })
+  const {
+    register: registerLegalName,
+    reset: resetLegalName,
+    handleSubmit: handleLegalNameSubmit,
+  } = useForm<IPersonalInfo>()
+  const { mutate: mutateLegalName, isPending: isPendingLegalName } =
+    useUpdatePersonalInfo(userId as number)
+  const queryClient = useQueryClient()
+
+  const onSubmitLegalName = (formData: IPersonalInfo) => {
+    const callBackReq = {
+      onSuccess: (data: any) => {
+        if (!data.error) {
+          queryClient.invalidateQueries({
+            queryKey: ["session"],
+          })
+          toast.success(data.message)
+          resetLegalName()
+        } else {
+          toast.error(String(data.message))
+        }
+      },
+      onError: (err: any) => {
+        toast.error(String(err))
+      },
+    }
+    mutateLegalName({ ...formData }, callBackReq)
+  }
+
+  return (
+    <div className="text-sm border-b border-text-50">
+      {!contentState.isButtonClicked ? (
+        <div className="flex justify-between pt-5 pb-8">
+          <div>
+            <Typography variant={"p"}>Value Added Tax (VAT)</Typography>
+            <Typography fontWeight={"light"}>
+              If you are VAT-registered, please add your VAT ID.
+            </Typography>
+          </div>
+          <button
+            onClick={() =>
+              setContentState({
+                isButtonClicked: !contentState.isButtonClicked,
+                contentId: "legalName",
+              })
+            }
+            className="underline self-start select-none "
+          >
+            Edit
+          </button>
+        </div>
+      ) : (
+        <div className="grid pt-5 pb-8">
+          <div className="flex justify-between">
+            <Typography variant={"p"}>Value Added Tax (VAT)</Typography>
+            <button
+              className="underline self-start select-none "
+              onClick={() =>
+                setContentState({
+                  isButtonClicked: !contentState.isButtonClicked,
+                  contentId: " ",
+                })
+              }
+            >
+              Cancel
+            </button>
+          </div>
+          <Typography fontWeight={"light"}>
+            If you are VAT-registered, please add your VAT ID.
+          </Typography>
+          <form onSubmit={handleLegalNameSubmit(onSubmitLegalName)}>
+            <div className="grid grid-cols-2 gap-4 my-4">
+              <Input
+                inputId="firstName"
+                inputLabel="VAT ID Number"
+                defaultValue={firstName}
+                {...registerLegalName("firstName")}
+              />
+              <Input
+                inputId="lastName"
+                inputLabel="Name on Registration"
+                defaultValue={lastName}
+                {...registerLegalName("lastName")}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4 my-4">
+              <Input
+                inputId="firstName"
+                inputLabel="Address line 1"
+                defaultValue={firstName}
+                {...registerLegalName("firstName")}
+              />
+              <Input
+                inputId="lastName"
+                inputLabel="Address line 2 (optional)"
+                defaultValue={lastName}
+                {...registerLegalName("lastName")}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4 my-4">
+              <Input
+                inputId="firstName"
+                inputLabel="City"
+                defaultValue={firstName}
+                {...registerLegalName("firstName")}
+              />
+              <Input
+                inputId="lastName"
+                inputLabel="Province"
+                defaultValue={lastName}
+                {...registerLegalName("lastName")}
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4 my-4">
+              <Input
+                inputId="firstName"
+                inputLabel="Zip/postal code"
+                defaultValue={firstName}
+                {...registerLegalName("firstName")}
+              />
+            </div>
+            <Button className="w-20" size={"sm"} type="submit">
+              {isPendingLegalName ? (
+                <div className="animate-spin w-4 h-4 border-2 border-current border-t-transparent text-primary-200 rounded-full">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                "Save"
+              )}
+            </Button>
+          </form>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Taxpayers
---
+++ apps/web/module/AccountSettings/Taxes/index.tsx
@@ -0,0 +1,70 @@
+"use client"
+import AccountSettingWrapper from "@/common/components/AccountSettingWrapper"
+import React, { useState } from "react"
+import { Breadcrumb } from "@/common/components/ui/Breadcrumb"
+import { Title } from "@/common/components/ui/Title"
+import { LINK_ACCOUNT_SETTINGS } from "@/common/constants/links"
+import combineClasses from "@/common/helpers/combineClasses"
+import Taxpayers from "./Taxpayers"
+import TaxDocuments from "./TaxDocuments"
+
+const Taxes = () => {
+  const [tableState, setTableState] = useState(0)
+  const TITLE = "Taxes"
+  let content
+
+  if (tableState === 0) {
+    content = (
+      <>
+        <Taxpayers/>
+      </>
+    )
+  } else if (tableState === 1) {
+    content = (
+      <>
+        <TaxDocuments/>
+      </>
+    )
+  }
+  return (
+    <AccountSettingWrapper>
+      <div>
+        <Breadcrumb
+          home="Account"
+          page={TITLE}
+          link={LINK_ACCOUNT_SETTINGS}
+        />
+        <Title>{TITLE}</Title>
+      </div>
+      <div className="hidden sm:block">
+        <div className="flex border-b border-b-text-50">
+          <button
+            className={combineClasses(
+              tableState === 0
+                ? "border-text-900 text-text-900"
+                : "border-transparent text-text-500 hover:border-text-300 hover:text-text-700",
+              "whitespace-nowrap border-b-2 py-4 px-8 text-sm font-medium"
+            )}
+            onClick={() => setTableState(0)}
+          >
+            Taxpayers
+          </button>
+          <button
+            className={combineClasses(
+              tableState === 1
+                ? "border-text-900 text-text-900"
+                : "border-transparent text-text-500 hover:border-text-300 hover:text-text-700",
+              "whitespace-nowrap border-b-2 py-4 px-8 text-sm font-medium"
+            )}
+            onClick={() => setTableState(1)}
+          >
+            Tax Documents
+          </button>
+        </div>
+        <div className="mt-6">{content}</div>
+      </div>
+    </AccountSettingWrapper>
+  )
+}
+
+export default Taxes
