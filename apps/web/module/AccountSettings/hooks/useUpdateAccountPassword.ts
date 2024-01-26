d1af6a198def4f1817a3514f6dabcbfa5ee46243
ramil25 <ramil25@users.noreply.github.com>
2024-01-26 04:22:54 +0000

    Prettified Code!

--- apps/web/module/AccountSettings/hooks/useUpdateAccountPassword.ts
+++ apps/web/module/AccountSettings/hooks/useUpdateAccountPassword.ts
@@ -3,15 +3,21 @@ import { ICoupon } from "@/common/types/global"
 import { API_URL_PAYMENTS, API_URL_USERS } from "@repo/constants"
 import { useMutation } from "@tanstack/react-query"
 
-interface IChangePassword{
-    currentPassword: string
-    newPassword: string,
-    confirmPassword: string
+interface IChangePassword {
+  currentPassword: string
+  newPassword: string
+  confirmPassword: string
 }
 
-export async function updateUserPassword(userId: number | null, props: IChangePassword) {
+export async function updateUserPassword(
+  userId: number | null,
+  props: IChangePassword
+) {
   const apiService = new ApiService()
-  return await apiService.patch(`${API_URL_USERS}/change-password/${userId}`, props)
+  return await apiService.patch(
+    `${API_URL_USERS}/change-password/${userId}`,
+    props
+  )
 }
 
 function useUpdateAccountPassword(userId: number | null) {
