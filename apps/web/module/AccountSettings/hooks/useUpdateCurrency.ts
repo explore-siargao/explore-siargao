33bcee8530816ac4b3ce874e5d09ff6198dfcbc2
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-01-26 07:11:16 +0000

    Prettified Code!

--- apps/web/module/AccountSettings/hooks/useUpdateCurrency.ts
+++ apps/web/module/AccountSettings/hooks/useUpdateCurrency.ts
@@ -2,9 +2,15 @@ import { ApiService } from "@/common/service/api"
 import { API_URL_USERS } from "@repo/constants"
 import { useMutation } from "@tanstack/react-query"
 
-export async function updateCurrency(personalInfoId: string | null, props: string) {
+export async function updateCurrency(
+  personalInfoId: string | null,
+  props: string
+) {
   const apiService = new ApiService()
-  return await apiService.patch(`${API_URL_USERS}/personal-info/currency/${personalInfoId}`, props)
+  return await apiService.patch(
+    `${API_URL_USERS}/personal-info/currency/${personalInfoId}`,
+    props
+  )
 }
 
 function useUpdateCurrency(personalInfoId: string | null) {
