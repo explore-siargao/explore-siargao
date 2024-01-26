fb599cb2e236fa6c573770ccc8767a07763072ee
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-26 13:49:28 +0800

    task(api): Created add tax api endpoint

---
+++ apps/api/src/routes/taxes/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { addTaxes } from "./services/addVatIdNumber";
+
+
+const router = express.Router()
+
+router.post('/:userId', addTaxes)
+
+export default router
