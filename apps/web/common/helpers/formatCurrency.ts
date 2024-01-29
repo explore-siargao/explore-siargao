9341bcb7d3d78b2e807c2baba9e7c315b80bab78
kylle080201 <kylle080201@users.noreply.github.com>
2024-01-29 02:26:17 +0000

    Prettified Code!

--- apps/web/common/helpers/formatCurrency.ts
+++ apps/web/common/helpers/formatCurrency.ts
@@ -1,12 +1,12 @@
 const formatCurrency = (amount: number, currency: string) => {
-  const formatter = new Intl.NumberFormat('en-PH', {
-      style: 'currency',
-      currency: currency,
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
-  });
+  const formatter = new Intl.NumberFormat("en-PH", {
+    style: "currency",
+    currency: currency,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  })
 
-  return formatter.format(amount);
+  return formatter.format(amount)
 }
 
 export default formatCurrency
