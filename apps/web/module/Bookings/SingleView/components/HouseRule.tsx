5da76d009da8e8c98f43b91cfeb0546f17590fb9
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-01-31 03:37:00 +0000

    Prettified Code!

--- apps/web/module/Bookings/SingleView/components/HouseRule.tsx
+++ apps/web/module/Bookings/SingleView/components/HouseRule.tsx
@@ -1,28 +1,27 @@
-import { Typography } from '@/common/components/ui/Typography'
-import React from 'react'
-
-
+import { Typography } from "@/common/components/ui/Typography"
+import React from "react"
 
 interface HouseRules {
-  title: string;
-  rules: string[];
+  title: string
+  rules: string[]
 }
 
 const HouseRule: React.FC<HouseRules> = ({ title, rules }) => {
-
   return (
     <div>
-       <Typography fontWeight={"semiBold"} className='px-10'>
-            {title}
-       </Typography>
-       <Typography className='px-10'>
-            <ul>
-                {rules.map((rule, index) => (
-                  // @ts-ignore
-                <li className='mt-2' key={index}>{rule.rule}</li>
-                ))}
-            </ul>
-       </Typography>
+      <Typography fontWeight={"semiBold"} className="px-10">
+        {title}
+      </Typography>
+      <Typography className="px-10">
+        <ul>
+          {rules.map((rule, index) => (
+            // @ts-ignore
+            <li className="mt-2" key={index}>
+              {rule.rule}
+            </li>
+          ))}
+        </ul>
+      </Typography>
     </div>
   )
 }
