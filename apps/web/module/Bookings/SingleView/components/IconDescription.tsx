d3547af087c51178723be93aec7513b9c53510e3
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-01-31 16:12:37 +0800

    task(web): Combined the Section UI and Icon Description

--- apps/web/module/Bookings/SingleView/components/IconDescription.tsx
+++ apps/web/module/Bookings/SingleView/components/IconDescription.tsx
@@ -2,17 +2,25 @@ import React from "react"
 
 interface IconDescriptionProps {
   icon: React.ElementType
-  desc: string
+  desc: React.ReactNode
+  isStrikeThrough?: boolean
 }
 
 const IconDescription: React.FC<IconDescriptionProps> = ({
   icon: Icon,
   desc,
+  isStrikeThrough = false
 }) => {
+  const IconDescriptionStyle = isStrikeThrough
+    ? { textDecoration: "line-through" }
+    : {}
+
   return (
-    <div className="flex items-center mb-2 gap-5">
+    <div className="flex items-center mb-5 gap-5">
       {Icon && <Icon className="w-6 h-6" />}
-      <div className="flex">{desc}</div>
+      <div className="flex" style={IconDescriptionStyle}>
+        {desc}
+      </div>
     </div>
   )
 }
--- apps/web/module/Bookings/index.tsx
+++ apps/web/module/Bookings/index.tsx
@@ -5,7 +5,6 @@ import { WidthWrapper } from "@/common/components/WidthWrapper"
 import useGetAllBookings from "../LandingPage/hooks/useGetAllBookings"
 import { Spinner } from "@/common/components/ui/Spinner"
 import useSessionStore from "@/common/store/useSessionStore"
-import { TitleSection } from "./SingleView/components/TitleSection"
 
 const Bookings = () => {
   const userId = useSessionStore((state) => state).id
