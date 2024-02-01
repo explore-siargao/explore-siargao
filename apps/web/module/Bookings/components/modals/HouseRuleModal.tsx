a278073de6ee98bac5053394a204f8eb2368058b
Arjay <arjayandal@gmail.com>
2024-02-01 16:05:55 +0800

    task(web): Added modal HouseRule modal

--- apps/web/module/Bookings/components/modals/HouseRuleModal.tsx
+++ apps/web/module/Bookings/components/modals/HouseRuleModal.tsx
@@ -30,6 +30,7 @@ const HouseRuleModal = ({
     <Transition.Root show={isOpen} as="div">
       <Dialog
         as="div"
+        id="houserulemodal"
         className="relative z-50"
         initialFocus={cancelButtonRef}
         onClose={onClose}
@@ -57,7 +58,7 @@ const HouseRuleModal = ({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform rounded-lg bg-white text-left shadow-xl transition-all w-full h-full md:w-1/2">
+              <Dialog.Panel id="houserulepanel" className="relative transform rounded-lg bg-white text-left shadow-xl transition-all w-full h-full md:w-1/2">
                 <ModalContainer title="" onClose={onClose}>
                   <div className="px-5 pt-4  md:h-[500px] md:overflow-y-auto">
                     <div className="font-semibold text-2xl pt-7">
