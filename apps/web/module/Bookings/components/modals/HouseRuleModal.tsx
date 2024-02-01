f793b2b4c610184db69b702539ae561fd4a57d69
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-02-01 08:06:19 +0000

    Prettified Code!

--- apps/web/module/Bookings/components/modals/HouseRuleModal.tsx
+++ apps/web/module/Bookings/components/modals/HouseRuleModal.tsx
@@ -58,7 +58,10 @@ const HouseRuleModal = ({
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel id="houserulepanel" className="relative transform rounded-lg bg-white text-left shadow-xl transition-all w-full h-full md:w-1/2">
+              <Dialog.Panel
+                id="houserulepanel"
+                className="relative transform rounded-lg bg-white text-left shadow-xl transition-all w-full h-full md:w-1/2"
+              >
                 <ModalContainer title="" onClose={onClose}>
                   <div className="px-5 pt-4  md:h-[500px] md:overflow-y-auto">
                     <div className="font-semibold text-2xl pt-7">
