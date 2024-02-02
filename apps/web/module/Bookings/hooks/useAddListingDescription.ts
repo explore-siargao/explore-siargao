3d022266ed1f2427f0a96ef371342d0b8599d0e3
Arjay <arjayandal@gmail.com>
2024-02-02 14:57:37 +0800

    feat(web): Added Listing Description hooks

---
+++ apps/web/module/Bookings/hooks/useAddListingDescription.ts
@@ -0,0 +1,19 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_BOOKINGS } from "@repo/constants"
+import { T_ListingDescription } from "@repo/contract"
+import { useMutation } from "@tanstack/react-query"
+
+export async function addDescription(userID: number ,listingId: number | undefined, props: T_ListingDescription) {
+  const apiService = new ApiService()
+  return await apiService.post(
+    `${API_URL_BOOKINGS}/${userID}/listing-description/${listingId}`,
+    props
+  )
+}
+function useAddListingDescription(userID: number, listingId: number) {
+  const query = useMutation({
+    mutationFn: (props: T_ListingDescription) => addDescription(userID, listingId, props),
+  })
+  return query
+}
+export default useAddListingDescription
