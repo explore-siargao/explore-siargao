492c5b41d3333dc5ccdee4a2267922c159a8cec3
ramil25 <ramil25@users.noreply.github.com>
2024-02-08 03:09:35 +0000

    Prettified Code!

--- apps/web/module/Bookings/SingleView/components/ContactHost.tsx
+++ apps/web/module/Bookings/SingleView/components/ContactHost.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/common/components/ui/Button"
 import { Textarea } from "@/common/components/ui/Textarea"
 import { Typography } from "@/common/components/ui/Typography"
-import {LucideChevronLeft } from "lucide-react"
+import { LucideChevronLeft } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -11,33 +11,38 @@ interface ContactHostProps {
   hostImage?: string
 }
 
-const ContactHost = ({hostName,listingName,hostImage}:ContactHostProps)=>{
-return(
+const ContactHost = ({
+  hostName,
+  listingName,
+  hostImage,
+}: ContactHostProps) => {
+  return (
     <div className="w-full">
-        <div className="py-8">
-    <Link href="">
-    <LucideChevronLeft size={50} strokeWidth={1} />
-    </Link>
-    </div>
-   <div className="flex w-full pb-6 border-b justify-between">
-    <div>
-    <Typography variant={"h2"}>Contact {hostName}</Typography>
-    <Typography className="text-gray-400">{listingName}</Typography>
-    </div>
-    <div>
-    {!hostImage ? (
-        <div className="bg-primary-400 w-16 h-16 rounded-full ring-1"></div>
-    ):(
-        
-        <Image src={hostImage as string} width={2} height={2} alt="image" />
-    )}
-    </div>
-   </div>
-   <div className="w-full py-6">
-    <Typography variant={"h2"}>Message the Host</Typography>
-    <Textarea className="h-40 my-6 focus:border-primary-700 focus:border-3 focus:ring-primary-700 " />
-    <Button variant={"outline"} size={"lg"} className="font-extrabold">Send Message</Button>
-   </div>
+      <div className="py-8">
+        <Link href="">
+          <LucideChevronLeft size={50} strokeWidth={1} />
+        </Link>
+      </div>
+      <div className="flex w-full pb-6 border-b justify-between">
+        <div>
+          <Typography variant={"h2"}>Contact {hostName}</Typography>
+          <Typography className="text-gray-400">{listingName}</Typography>
+        </div>
+        <div>
+          {!hostImage ? (
+            <div className="bg-primary-400 w-16 h-16 rounded-full ring-1"></div>
+          ) : (
+            <Image src={hostImage as string} width={2} height={2} alt="image" />
+          )}
+        </div>
+      </div>
+      <div className="w-full py-6">
+        <Typography variant={"h2"}>Message the Host</Typography>
+        <Textarea className="h-40 my-6 focus:border-primary-700 focus:border-3 focus:ring-primary-700 " />
+        <Button variant={"outline"} size={"lg"} className="font-extrabold">
+          Send Message
+        </Button>
+      </div>
     </div>
   )
 }
