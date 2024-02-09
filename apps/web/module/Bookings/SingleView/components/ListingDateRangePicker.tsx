dbeb1ffe1aa680d77c7a8722642866b9078343fa
kinnethjavier <kinnethandrew9@gmail.com>
2024-02-09 17:02:36 +0800

    feat(web): Added date range UI

---
+++ apps/web/module/Bookings/SingleView/components/ListingDateRangePicker.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { useState } from "react"
+import DateRangePicker from "../../../../common/components/DateRangePicker"
+import { DateRange } from "react-day-picker"
+import format from "date-fns/format"
+import { Typography } from "@/common/components/ui/Typography"
+import { Button } from "@/common/components/ui/Button"
+
+interface ListingDRProps {
+    title: string
+}
+
+const ListingDateRangePicker = ({title}: ListingDRProps) => {
+    const [date, setDate] = useState<DateRange | undefined>({
+        from: undefined,   
+        to: undefined,
+    })
+    return (
+        <div className="md:w-[477px]">
+            <Typography variant="h3" fontWeight="semiBold" className="mb-1">{title}</Typography>
+            <Typography variant="h6" className="mb-4">{date?.from != undefined ? format(date.from, "LLL dd, y") : "Date from"} - {date?.to != undefined ? format(date.to, "LLL dd, y") : "Date to"}</Typography>
+            <DateRangePicker date={date} setDate={setDate}/>
+            <Button variant="ghost" className="underline md:float-right" size="sm" onClick={() => setDate(undefined)}>Clear dates</Button>
+        </div>
+    )
+}
+
+export default ListingDateRangePicker
