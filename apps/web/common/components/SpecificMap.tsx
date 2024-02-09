6fb6b10fb572619898f3e66b9e1a736bfb5b5f2d
kinnethjavier <kinnethjavier@users.noreply.github.com>
2024-02-09 02:45:27 +0000

    Prettified Code!

--- apps/web/common/components/SpecificMap.tsx
+++ apps/web/common/components/SpecificMap.tsx
@@ -3,55 +3,60 @@ import "leaflet/dist/leaflet.css"
 import { Icon } from "leaflet"
 
 interface SpecificMapProps {
-    coordinates: [Number, Number]
-    mapHeight: string
-    mapWidth: string
+  coordinates: [Number, Number]
+  mapHeight: string
+  mapWidth: string
 }
 
 const homeIcon = new Icon({
-    iconUrl: "https://www.nicepng.com/png/full/115-1153942_white-home-icon-png-white-home-logo-transparent.png",
-    iconSize: [35, 35],
-    iconAnchor: [18, 18],
-    popupAnchor: [-3, -76]
+  iconUrl:
+    "https://www.nicepng.com/png/full/115-1153942_white-home-icon-png-white-home-logo-transparent.png",
+  iconSize: [35, 35],
+  iconAnchor: [18, 18],
+  popupAnchor: [-3, -76],
 })
 
-const SpecificMap = ({coordinates, mapHeight, mapWidth}: SpecificMapProps) => {
-    return (
-        <div className="flex-1 hidden lg:block">
-            <div className={`${mapHeight} ${mapWidth}`}>
-                <MapContainer
-                //@ts-ignore
-                center={coordinates}
-                zoom={15}
-                scrollWheelZoom={true}
-                style={{
-                    height: "100%",
-                    width: "100%",
-                    zIndex: 30,
-                }}
-                >
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-    
-                    <CircleMarker 
-                    // @ts-ignore
-                    center={coordinates} 
-                    color="#fc2d46"
-                    fillColor="#fc2d46"
-                    fillOpacity={1}
-                    radius={30}>
-                    </CircleMarker>
-                    <Marker 
-                    //@ts-ignore
-                    position={coordinates}
-                    icon={homeIcon}>
-                    </Marker>
-                </MapContainer>
-            </div>
-        </div>
-    )
+const SpecificMap = ({
+  coordinates,
+  mapHeight,
+  mapWidth,
+}: SpecificMapProps) => {
+  return (
+    <div className="flex-1 hidden lg:block">
+      <div className={`${mapHeight} ${mapWidth}`}>
+        <MapContainer
+          //@ts-ignore
+          center={coordinates}
+          zoom={15}
+          scrollWheelZoom={true}
+          style={{
+            height: "100%",
+            width: "100%",
+            zIndex: 30,
+          }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <CircleMarker
+            // @ts-ignore
+            center={coordinates}
+            color="#fc2d46"
+            fillColor="#fc2d46"
+            fillOpacity={1}
+            radius={30}
+          ></CircleMarker>
+          <Marker
+            //@ts-ignore
+            position={coordinates}
+            icon={homeIcon}
+          ></Marker>
+        </MapContainer>
+      </div>
+    </div>
+  )
 }
 
 export default SpecificMap
