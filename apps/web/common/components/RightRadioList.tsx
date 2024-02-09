56799858e6fed48ce66a99cdf6c5a9e33ae12374
Arjay M. Andal <arjayandal93@gmail.com>
2024-02-09 13:54:30 +0800

    feat(web): Added Modal for reporting

--- apps/web/common/components/RightRadioList.tsx
+++ apps/web/common/components/RightRadioList.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { Title } from "./ui/Title"
 import { Typography } from "./ui/Typography"
 
 interface RightRadioListProps {
   title: string
-  lists: { id: number | string; option: string }[]
+  lists: {
+     id: number | string; option: string; description?: string 
+}[]
+  defaultSelectedId?: number | string;
+  description?: string
+  onSelect: (option: string) => void;
 }
 
-const RightRadioList = ({ title, lists }: RightRadioListProps) => {
+const RightRadioList: React.FC<RightRadioListProps> = ({title, lists, onSelect, defaultSelectedId }) => {
+
+  const [selectedOption, setSelectedOption] = useState(defaultSelectedId || '');
+
+  const handleOptionChange = (event: { target: { value: any; }; }) => {
+    const value = event.target.value;
+    setSelectedOption(value);
+    onSelect(value);
+  };
+
   return (
     <div className="flex flex-col w-full">
       <Title size="sub" className="w-full py-3 border-b">
@@ -15,13 +30,24 @@ const RightRadioList = ({ title, lists }: RightRadioListProps) => {
       {lists.map((choice) => (
         <div key={choice.id} className="flex w-full py-3 border-b">
           <label className="flex items-center w-full">
-            <Typography>{choice.option}</Typography>
+            <div>
+            <Typography className="text-gray-600">{choice.option}</Typography>
+            <div className="items-start">
+              {choice.description && (
+                  <Typography className="text-gray-400">{choice.description}</Typography>
+                )}
+            </div>
+            </div>
+           
             <input
               name="helper-radio"
               type="radio"
               value={choice.option}
+              checked={selectedOption === choice.option}
+              onChange={handleOptionChange}
               className="ml-auto w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             />
+             
           </label>
         </div>
       ))}
