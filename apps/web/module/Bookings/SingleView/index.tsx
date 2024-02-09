a7b58fdcaded31cc234d8c7528e10203b50a8791
ramil25 <ramil25@users.noreply.github.com>
2024-02-09 01:30:30 +0000

    Prettified Code!

--- apps/web/module/Bookings/SingleView/index.tsx
+++ apps/web/module/Bookings/SingleView/index.tsx
@@ -1,10 +1,6 @@
 import { Title } from "@/common/components/ui/Title"
 
 export const SingleView = () => {
-  return (
-    
-      <Title className="mt-36">Accomodation</Title>
-    
-  )
+  return <Title className="mt-36">Accomodation</Title>
 }
 export default SingleView
