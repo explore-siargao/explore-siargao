ad67272936432b1fbc80e64893b00a89eebe47c0
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-02-11 19:14:51 +0800

    feat: Added place offer section in accommodation

--- apps/web/module/Bookings/SingleView/components/PlaceOffers.tsx
+++ apps/web/module/Bookings/SingleView/components/PlaceOffers.tsx
@@ -11,29 +11,39 @@ import {
   LucideCigaretteOff,
   LucideWifi,
 } from "lucide-react"
+import PlaceOfferModal from "../../components/modals/PlaceOfferModal"
+import { useState } from "react"
 
 const PlaceOffers = () => {
+  const [showMoreModalOpen, setShowMoreModalOpen] = useState(false)
   return (
-    <TitleSection size="lg" title="What this place offers">
-      <div className="mb-5"></div>
-      <div className="grid grid-cols-2">
-        <IconDescription icon={LucideWifi} desc="WiFi" />
-        <IconDescription icon={LucideCarFront} desc="Free street parking" />
-        <IconDescription icon={LucideCigaretteOff} desc="No smoking" />
-        <IconDescription icon={LucideAlarmSmoke} desc="Smoke alarm" />
-        <IconDescription icon={LucideBed} desc="Bed" />
-        <IconDescription icon={LucideAngry} desc="Angry people" />
-        <IconDescription
-          icon={LucideAlarmClock}
-          desc="Alarm clock"
-          isNotIncluded
-        />
-        <IconDescription icon={LucideBug} desc="No bugs" />
-      </div>
-      <Button className="mt-5" variant={"shaded"}>
-        Show all 56 amenities
-      </Button>
-    </TitleSection>
+    <>
+      <TitleSection size="lg" title="What this place offers">
+        <div className="mb-5"></div>
+        <div className="grid grid-cols-2">
+          <IconDescription icon={LucideWifi} desc="WiFi" />
+          <IconDescription icon={LucideCarFront} desc="Free street parking" />
+          <IconDescription icon={LucideCigaretteOff} desc="No smoking" />
+          <IconDescription icon={LucideAlarmSmoke} desc="Smoke alarm" />
+          <IconDescription icon={LucideBed} desc="Bed" />
+          <IconDescription icon={LucideAngry} desc="Angry people" />
+          <IconDescription
+            icon={LucideAlarmClock}
+            desc="Alarm clock"
+            isNotIncluded
+          />
+          <IconDescription icon={LucideBug} desc="No bugs" />
+        </div>
+        <Button className="mt-5" variant={"shaded"} onClick={() => setShowMoreModalOpen(!showMoreModalOpen)}>
+          Show all 56 amenities
+        </Button>
+      </TitleSection>
+      <PlaceOfferModal
+        isOpen={showMoreModalOpen}
+        onClose={() => setShowMoreModalOpen(!showMoreModalOpen)}
+        datas={[]}
+      />
+    </>
   )
 }
 
