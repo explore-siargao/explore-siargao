ab702a44dbadb6a1edd562b55ae2797286f9e77c
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-02-12 17:30:26 +0800

    feat: Added message host

--- apps/web/module/Bookings/SingleView/Message.tsx
+++ apps/web/module/Bookings/SingleView/Message.tsx
@@ -1,10 +1,17 @@
 import { WidthWrapper } from "@/common/components/WidthWrapper"
 import ContactHost from "./components/ContactHost"
 import CheckoutProcess from "./components/CheckoutProcess"
+import { LucideChevronLeft } from "lucide-react"
+import Link from "next/link"
 
 const Message = () => {
   return (
-    <WidthWrapper width="medium" className="mt-24 lg:mt-28">
+    <WidthWrapper width="small" className="mt-24 lg:mt-28">
+      <div className="pb-10 pt-6">
+        <Link href="/accommodation/1">
+          <LucideChevronLeft size={20}  />
+        </Link>
+      </div>
       <div className="flex flex-col md:flex-row gap-8 md:gap-24 pb-6">
         <div className="flex-1">
           <ContactHost hostName="Ramil" listingName="test test" />
