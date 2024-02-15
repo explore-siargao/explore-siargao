f174e4a83338ca2b9ba3a4d80f0ea44ddf64af4e
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-02-15 12:03:22 +0800

    feat(web): Created a modal with title and image

---
+++ apps/web/module/AccountSettings/components/modals/GovernmentIdModal.tsx
@@ -0,0 +1,36 @@
+import ModalContainer from "@/common/components/ModalContainer"
+import { Typography } from "@/common/components/ui/Typography"
+import Image from "next/image"
+
+interface GovernmentIdModalProps {
+  isOpen: boolean
+  onClose: () => void
+  fileKey: string[]
+  title: string
+}
+
+const GovernmentIdModal = ({
+  isOpen,
+  onClose,
+  fileKey,
+  title,
+}: GovernmentIdModalProps) => {
+  return (
+    <ModalContainer isOpen={isOpen} onClose={onClose} size="sm">
+      <div className="relative space-y-3 pt-3 pb-3 h-[250px] overflow-y-auto">
+        <Typography variant={"h2"} className="flex justify-center">
+          {title}
+        </Typography>{" "}
+        {fileKey.map((data) => {
+          return (
+            <div className="relative h-[204px] w-[324px] mx-auto bg-gray-200">
+              <Image src={data} layout="fill" objectFit="cover" alt="" />
+            </div>
+          )
+        })}
+      </div>
+    </ModalContainer>
+  )
+}
+
+export default GovernmentIdModal
