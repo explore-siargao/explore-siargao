2c90ee77ac813f76c144fed10bc223a8c6d3470b
kinnethjavier <kinnethjavier@users.noreply.github.com>
2024-02-15 05:17:36 +0000

    Prettified Code!

--- apps/web/common/store/useGuestAdd.ts
+++ apps/web/common/store/useGuestAdd.ts
@@ -1,36 +1,34 @@
 import { create } from "zustand"
 
 type T_Guest = {
-    adults: number
-    children: number
-    infants: number 
+  adults: number
+  children: number
+  infants: number
 }
 
 type T_GuestState = {
-    guest: T_Guest
-    updateGuest: (updatedGuest: Partial<T_Guest>) => void
-    incrementGuest: (type: keyof T_Guest) => void
+  guest: T_Guest
+  updateGuest: (updatedGuest: Partial<T_Guest>) => void
+  incrementGuest: (type: keyof T_Guest) => void
 }
 
-const useGuestAdd = create<T_GuestState>(
-    (set) => ({
-        guest: {
-            adults: 0,
-            children: 0,
-            infants: 0
-        },
-        updateGuest: (updatedGuest) =>
-        set((state) => ({
-            guest: { ...state.guest, ...updatedGuest },
-        })),
-        incrementGuest: (type) =>
-        set((state) => ({
-            guest: {
-                ...state.guest,
-                [type]: state.guest[type] + 1,
-            },
-        })),
-    })
-)
+const useGuestAdd = create<T_GuestState>((set) => ({
+  guest: {
+    adults: 0,
+    children: 0,
+    infants: 0,
+  },
+  updateGuest: (updatedGuest) =>
+    set((state) => ({
+      guest: { ...state.guest, ...updatedGuest },
+    })),
+  incrementGuest: (type) =>
+    set((state) => ({
+      guest: {
+        ...state.guest,
+        [type]: state.guest[type] + 1,
+      },
+    })),
+}))
 
 export default useGuestAdd
--- apps/web/module/Bookings/SingleView/components/modals/GuestAddModal.tsx
+++ apps/web/module/Bookings/SingleView/components/modals/GuestAddModal.tsx
@@ -4,57 +4,90 @@ import useGuestAdd from "@/common/store/useGuestAdd"
 import { PlusCircle } from "lucide-react"
 
 interface GuestAddModalProps {
-    isOpen: boolean
-    onClose: () => void
+  isOpen: boolean
+  onClose: () => void
 }
 
-const GuestAddModal = ({isOpen, onClose}: GuestAddModalProps) => {
-    const guest = useGuestAdd((state) => state.guest)
-    const incrementGuest = useGuestAdd((state) => state.incrementGuest)
+const GuestAddModal = ({ isOpen, onClose }: GuestAddModalProps) => {
+  const guest = useGuestAdd((state) => state.guest)
+  const incrementGuest = useGuestAdd((state) => state.incrementGuest)
 
-    return (
-        <ModalContainer isOpen={isOpen} onClose={onClose} size="sm">
-            <div className="py-2">
-                <div className="flex justify-between items-center px-4 py-2">
-                    <div className="space-y-0.5">
-                        <Typography variant="h4" fontWeight="bold">Adults</Typography>
-                        <Typography variant="h5" fontWeight="semibold">Age 13+</Typography>
-                    </div>
-                    <div className="flex items-center space-x-3">
-                        <Typography variant="h5" fontWeight="semibold" className="mt-1">{guest.adults}</Typography>
-                        <button  onClick={() => {incrementGuest("adults")}}>
-                            <PlusCircle className="h-8 w-8" strokeWidth={0.5}/>
-                        </button>
-                    </div>
-                </div>
-                <div className="flex justify-between items-center px-4 py-2">
-                    <div className="space-y-0.5">
-                        <Typography variant="h4" fontWeight="bold">Children</Typography>
-                        <Typography variant="h5" fontWeight="semibold">Ages 2-12</Typography>
-                    </div>
-                    <div className="flex items-center space-x-3">
-                        <Typography variant="h5" fontWeight="semibold" className="mt-1">{guest.children}</Typography>
-                        <button onClick={() => {incrementGuest("children")}}>
-                            <PlusCircle className="h-8 w-8" strokeWidth={0.5}/>
-                        </button>
-                    </div>
-                </div>
-                <div className="flex justify-between items-center px-4 py-2">
-                    <div className="space-y-0.5">
-                        <Typography variant="h4" fontWeight="bold">Infants</Typography>
-                        <Typography variant="h5" fontWeight="semibold">Under 2</Typography>
-                    </div>
-                    <div className="flex items-center space-x-3">
-                        <Typography variant="h5" fontWeight="semibold" className="mt-1">{guest.infants}</Typography>
-                        <button onClick={() => {incrementGuest("infants")}}>
-                            <PlusCircle className="h-8 w-8" strokeWidth={0.5}/>
-                        </button>
-                    </div>
-                </div>
-                <Typography variant="h6" className="px-4 py-2">This place has a maximum of 8 guests, not including infants. Pets aren't allowed.</Typography>
-            </div>
-        </ModalContainer>
-    )
+  return (
+    <ModalContainer isOpen={isOpen} onClose={onClose} size="sm">
+      <div className="py-2">
+        <div className="flex justify-between items-center px-4 py-2">
+          <div className="space-y-0.5">
+            <Typography variant="h4" fontWeight="bold">
+              Adults
+            </Typography>
+            <Typography variant="h5" fontWeight="semibold">
+              Age 13+
+            </Typography>
+          </div>
+          <div className="flex items-center space-x-3">
+            <Typography variant="h5" fontWeight="semibold" className="mt-1">
+              {guest.adults}
+            </Typography>
+            <button
+              onClick={() => {
+                incrementGuest("adults")
+              }}
+            >
+              <PlusCircle className="h-8 w-8" strokeWidth={0.5} />
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-4 py-2">
+          <div className="space-y-0.5">
+            <Typography variant="h4" fontWeight="bold">
+              Children
+            </Typography>
+            <Typography variant="h5" fontWeight="semibold">
+              Ages 2-12
+            </Typography>
+          </div>
+          <div className="flex items-center space-x-3">
+            <Typography variant="h5" fontWeight="semibold" className="mt-1">
+              {guest.children}
+            </Typography>
+            <button
+              onClick={() => {
+                incrementGuest("children")
+              }}
+            >
+              <PlusCircle className="h-8 w-8" strokeWidth={0.5} />
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center px-4 py-2">
+          <div className="space-y-0.5">
+            <Typography variant="h4" fontWeight="bold">
+              Infants
+            </Typography>
+            <Typography variant="h5" fontWeight="semibold">
+              Under 2
+            </Typography>
+          </div>
+          <div className="flex items-center space-x-3">
+            <Typography variant="h5" fontWeight="semibold" className="mt-1">
+              {guest.infants}
+            </Typography>
+            <button
+              onClick={() => {
+                incrementGuest("infants")
+              }}
+            >
+              <PlusCircle className="h-8 w-8" strokeWidth={0.5} />
+            </button>
+          </div>
+        </div>
+        <Typography variant="h6" className="px-4 py-2">
+          This place has a maximum of 8 guests, not including infants. Pets
+          aren't allowed.
+        </Typography>
+      </div>
+    </ModalContainer>
+  )
 }
 
 export default GuestAddModal
