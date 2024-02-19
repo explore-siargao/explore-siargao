416f4e3a4e6f4846409c86cf685d0deca9dac767
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-02-19 14:58:34 +0800

    feat(web): Created reusable tabs

--- apps/web/common/components/Tabs.tsx
+++ apps/web/common/components/Tabs.tsx
@@ -1,43 +1,32 @@
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 interface Tab {
   name: string
   icon?: JSX.Element
   link: string
+  isSelected?: boolean
 }
 
 interface TabsProps {
   tabs: Tab[]
-  activeTab: string
-  onTabChange: (tabName: string) => void
 }
 
-const Tabs = ({ tabs, activeTab, onTabChange }: TabsProps) => {
-  const handleTabClick = (tabName: string) => {
-    alert(`Selected ${tabName}`)
-    onTabChange(tabName)
-  }
+export default function Tabs({ tabs }: TabsProps) {
+  const currentPath = usePathname()
 
   return (
     <div>
       <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
-          Select a tab
-        </label>
         <select
           id="tabs"
           name="tabs"
           className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm"
-          value={activeTab}
-          onChange={(e) => {
-            const selectedTabName = e.target.value
-            handleTabClick(selectedTabName)
-            const selectedTab = tabs.find((tab) => tab.name === selectedTabName)
-            if (selectedTab) window.location.href = selectedTab.link
-          }}
+          value={currentPath}
+          onChange={(e) => (window.location.href = e.target.value)}
         >
           {tabs.map((tab) => (
-            <option key={tab.name} value={tab.name}>
+            <option key={tab.name} value={tab.link} selected={tab.isSelected}>
               {tab.name}
             </option>
           ))}
@@ -47,23 +36,17 @@ const Tabs = ({ tabs, activeTab, onTabChange }: TabsProps) => {
         <div className="border-b border-gray-200">
           <nav className="-mb-px flex space-x-8" aria-label="Tabs">
             {tabs.map((tab) => (
-              <Link
-                key={tab.name}
-                href={tab.link}
-                passHref={true}
-                legacyBehavior={true}
-              >
-                <a
-                  onClick={() => handleTabClick(tab.name)}
+              <Link key={tab.name} href={tab.link} passHref={true}>
+                <div
                   className={`flex items-center whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium ${
-                    tab.name === activeTab
+                    tab.isSelected || currentPath === tab.link
                       ? "border-primary-500 text-primary-600"
-                      : "border-transparent text-gray-500 hover:border-primary-300 hover:text-primary-700"
+                      : "border-transparent text-gray-500 hover:border-primary-500 hover:text-primary-600"
                   }`}
                 >
                   {tab.icon && <span className="mr-1">{tab.icon}</span>}
                   {tab.name}
-                </a>
+                </div>
               </Link>
             ))}
           </nav>
@@ -72,5 +55,3 @@ const Tabs = ({ tabs, activeTab, onTabChange }: TabsProps) => {
     </div>
   )
 }
-
-export default Tabs
