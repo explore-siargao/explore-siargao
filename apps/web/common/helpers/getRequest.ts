1b0993b26f68704145aa716844c35151438b27f8
jpmadrigal07 <jpmadrigal07@users.noreply.github.com>
2024-02-20 00:46:38 +0000

    Prettified Code!

--- apps/web/common/helpers/getRequest.ts
+++ apps/web/common/helpers/getRequest.ts
@@ -1,13 +1,13 @@
-import { API_ROOT } from "@repo/constants";
+import { API_ROOT } from "@repo/constants"
 
-const CACHE_REVALIDATE = 60;
+const CACHE_REVALIDATE = 60
 export async function getRequest(url: string) {
   const res = await fetch(`${process.env.API_URL}${API_ROOT}${url}`, {
     next: { revalidate: CACHE_REVALIDATE },
-  });
+  })
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed to fetch data")
   }
-  const jsonData = await res.json();
-  return jsonData;
+  const jsonData = await res.json()
+  return jsonData
 }
