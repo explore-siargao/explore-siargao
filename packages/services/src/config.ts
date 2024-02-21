96d259853f9923ede0fc093d14aef4500657a498
ramil25 <ramil25@users.noreply.github.com>
2024-02-21 05:01:42 +0000

    Prettified Code!

--- packages/services/src/config.ts
+++ packages/services/src/config.ts
@@ -1,6 +1,6 @@
-import dotenv from 'dotenv'
-import path from 'path'
+import dotenv from "dotenv"
+import path from "path"
 
-dotenv.config({ path: path.join(__dirname, '../../../.env') })
+dotenv.config({ path: path.join(__dirname, "../../../.env") })
 
-export const encryptKey = process.env.API_ENCRYPT_KEY || ''
+export const encryptKey = process.env.API_ENCRYPT_KEY || ""
