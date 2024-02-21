5f2578e18c3aa2aff60a873abe976476961b0461
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-02-21 13:10:25 +0800

    feat: Added xendit payment

--- apps/api/src/routes/xendit/services/default.ts
+++ apps/api/src/routes/xendit/services/default.ts
@@ -1,8 +1,8 @@
-import { xenditUrl } from '@/common/config';
 import { ApiService } from '@/common/service/api';
 import { ResponseService } from '@/common/service/response';
 import { Response, Request } from 'express'
 import { randomUUID } from 'crypto'
+import { REQUIRED_VALUE_EMPTY } from '@/common/constants';
 
 const response = new ResponseService()
 const apiXendit = new ApiService("xendit")
@@ -26,10 +26,6 @@ export const cardSingleUse = async (req: Request, res: Response) => {
         }
       },
       "reusability": "ONE_TIME_USE",
-      "description": "This is a description.",
-      "metadata": {
-        "foo": "bar"
-      }
     }
     const req = await apiXendit.post(`/v2/payment_methods`, data, false, true);
     return res.json(response.success({ item: req }))
@@ -58,10 +54,6 @@ export const cardMultiUse = async (req: Request, res: Response) => {
         }
       },
       "reusability": "MULTIPLE_USE",
-      "description": "This is a description.",
-      "metadata": {
-        "foo": "bar"
-      }
     }
     const req = await apiXendit.post(`/v2/payment_methods`, data, false, true);
     return res.json(response.success({ item: req }))
@@ -71,34 +63,70 @@ export const cardMultiUse = async (req: Request, res: Response) => {
 }
 
 export const cardCreatePayment = async (req: Request, res: Response) => {
-  try {
-    const data = {
-      "amount": 15000,
-      "currency": "PHP",
-      "payment_method_id": "<linked payment method id>",
-      "description": "This is a description.",
-      "metadata": {
-          "foo": "bar"
-      },
-      "capture_method": "MANUAL"
+  const { paymentMethodId, amount } = req.body
+  if(paymentMethodId && amount) {
+    try {
+      const data = {
+        "amount": amount,
+        "currency": "PHP",
+        "payment_method_id": paymentMethodId,
+        "capture_method": "MANUAL"
+      }
+      const req = await apiXendit.post(`/payment_requests`, data, false, true);
+      return res.json(response.success({ item: req }))
+    } catch (err: any) {
+      return res.json(response.error({ message: err.message }))
     }
-    const req = await apiXendit.post(`/payment_requests`, data, false, true);
-    return res.json(response.success({ item: req }))
-  } catch (err: any) {
-    return res.json(response.error({ message: err.message }))
+  } else {
+    return res.json(response.error({ message: REQUIRED_VALUE_EMPTY }))
   }
 }
 
 export const cardInitiatePayment = async (req: Request, res: Response) => {
-  try {
-    const data = {
-      "capture_amount": 1500,
-      "reference_id": `capture-reference-${randomUUID}`
+  const { paymentRequestId } = req.body
+  if(paymentRequestId) {
+    try {
+      const data = {
+        "capture_amount": 1500,
+        "reference_id": `capture-reference-${randomUUID}`
+      }
+      const req = await apiXendit.post(`/payment_requests/${paymentRequestId}/captures`, data, false, true);
+      return res.json(response.success({ item: req }))
+    } catch (err: any) {
+      return res.json(response.error({ message: err.message }))
     }
-    const req = await apiXendit.post(`/payment_requests/:id/captures`, data, false, true);
-    return res.json(response.success({ item: req }))
-  } catch (err: any) {
-    return res.json(response.error({ message: err.message }))
+  } else {
+    return res.json(response.error({ message: REQUIRED_VALUE_EMPTY }))
+  }
+}
+
+export const gcashCreatePayment = async (req: Request, res: Response) => {
+  const { amount } = req.body
+  if(amount) {
+    try {
+      const data = {
+        "amount": amount,
+        "currency": "PHP",
+        "country": "PH",
+        "payment_method": {
+            "type": "EWALLET",
+            "ewallet": {
+                "channel_code": "GCASH",
+                "channel_properties": {
+                    "success_return_url": "https://redirect.me/goodstuff",
+                    "failure_return_url": "https://redirect.me/goodstuff"
+                }
+            },
+            "reusability": "ONE_TIME_USE"
+        }
+    }
+      const req = await apiXendit.post(`/payment_requests`, data, false, true);
+      return res.json(response.success({ item: req }))
+    } catch (err: any) {
+      return res.json(response.error({ message: err.message }))
+    }
+  } else {
+    return res.json(response.error({ message: REQUIRED_VALUE_EMPTY }))
   }
 }
 
