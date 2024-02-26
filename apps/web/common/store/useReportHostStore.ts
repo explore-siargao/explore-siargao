113b49854b0d2169adff0124d2c5e25bd59c5b40
kinnethjavier <kinnethandrew9@gmail.com>
2024-02-26 15:14:56 +0800

    feat(web): Report host modal structure changed

--- apps/web/common/store/useReportHostStore.ts
+++ apps/web/common/store/useReportHostStore.ts
@@ -1,98 +1,42 @@
 import { create } from "zustand"
 
 type T_ReportHostState = {
-  pageLevel: number
-  firstLevelIndex: number
-  secondLevelIndex: number
-  data: {
-    name: string
-    reason: string
-    description: string
-    listingId: number | null
-    reportedBy: number | null
-  }
-  isReported: boolean
-  increasePageLevel: () => void
-  decreasePageLevel: () => void
-  setFirstLevelIndex: (value: number) => void
-  setSecondLevelIndex: (value: number) => void
-  setFirstLevel: (value: string) => void
-  setSecondLevel: (value: string) => void
-  setDescription: (value: string) => void
-  setMultipleDescription: (value: string) => void
-  setRemoveDescription: (value: string) => void
-  setClearDescription: () => void
-  setIsReported: () => void
+  currentContent: string | null
+  output: string[]
+  subTitle: string
+  placeholder: string
+  selectables: string[]
+  setCurrentContent: (value: string | null) => void
+  setOutput: (value: string[]) => void 
+  removeLastValue: () => void
+  setSubTitle: (value: string) => void
+  setPlaceHolder: (value: string) => void
+  setSelectables: (value: string[]) => void
 }
 
 const useReportHostStore = create<T_ReportHostState>((set) => ({
-  pageLevel: 1,
-  firstLevelIndex: 0,
-  secondLevelIndex: 0,
-  data: {
-    name: "",
-    reason: "",
-    description: "",
-    listingId: null,
-    reportedBy: null,
-  },
-  isReported: false,
-  increasePageLevel: () => set((state) => ({ pageLevel: state.pageLevel + 1 })),
-  decreasePageLevel: () => set((state) => ({ pageLevel: state.pageLevel - 1 })),
-  setFirstLevelIndex: (value) => set((state) => ({ firstLevelIndex: value })),
-  setSecondLevelIndex: (value) => set((state) => ({ secondLevelIndex: value })),
-  setFirstLevel: (value) =>
-    set((state) => ({
-      data: {
-        ...state.data,
-        name: value,
-      },
-    })),
-  setSecondLevel: (value) =>
-    set((state) => ({
-      data: {
-        ...state.data,
-        reason: value,
-      },
-    })),
-  setDescription: (value) =>
-    set((state) => ({
-      data: {
-        ...state.data,
-        description: value,
-      },
-    })),
-  setMultipleDescription: (value) =>
-    set((state) => ({
-      data: {
-        ...state.data,
-        description: state.data.description
-          ? state.data.description + ", " + value
-          : value,
-      },
-    })),
-  setRemoveDescription: (value) =>
-    set((state) => ({
-      data: {
-        ...state.data,
-        description: state.data.description
-          .split(", ")
-          .filter((item) => item !== value)
-          .join(", "),
-      },
-    })),
-  setClearDescription: () =>
-    set((state) => ({
-      data: {
-        ...state.data,
-        description: "",
-      },
-    })),
-  setIsReported: () =>
-    set((state) => ({
-      isReported: !state.isReported,
-      pageLevel: 1,
-    })),
+  currentContent: null,
+  output: [],
+  subTitle: "",
+  placeholder: "",
+  selectables: [], 
+  setCurrentContent: (value) => set(() => ({currentContent: value})),
+  setOutput: (value) => set((state) => ({
+    ...state.output,
+    output: [...state.output, ...value]
+  })),
+  removeLastValue: () => set((state) => ({
+    output: state.output.splice(0, state.output.length - 1)
+  })),
+  setSubTitle: (value) => set(() => ({
+    subTitle: value
+  })),
+  setPlaceHolder: (value) => set(() => ({
+    placeholder: value
+  })),
+  setSelectables: (value) => set(() => ({
+    selectables: value
+  }))
 }))
 
 export default useReportHostStore
