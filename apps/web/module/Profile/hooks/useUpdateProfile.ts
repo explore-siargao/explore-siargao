066b416428c7f9a6cc50640b39d37e0e29b0afbf
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-02-27 16:53:57 +0800

    feat(api): Added endpoint for edit and get profile and hooks

---
+++ apps/web/module/Profile/hooks/useUpdateProfile.ts
@@ -0,0 +1,37 @@
+import { ApiService } from "@/common/service/api"
+import { API_URL_USERS } from "@/common/constants"
+import { useMutation } from "@tanstack/react-query"
+
+interface ProfileProps {
+school?:string
+work?:string
+live?:string
+language?:string
+born?:string
+favoriteSong?:string
+obsessedWith?:string,
+funFact?:string
+uselessSkill?:string
+biography?:string
+spendTime?:string
+pets?:string
+aboutMe?:string
+}
+export async function updateProfile(
+  userId: number | null,
+  props: ProfileProps
+) {
+  const apiService = new ApiService()
+  return await apiService.patch(
+    `${API_URL_USERS}/${userId}/profile`,
+    props
+  )
+}
+
+function useUpdateProfile(userId: number | null) {
+  const query = useMutation({
+    mutationFn: (props: ProfileProps) => updateProfile(userId, props),
+  })
+  return query
+}
+export default useUpdateProfile
