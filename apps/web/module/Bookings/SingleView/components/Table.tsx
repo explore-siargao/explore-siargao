8c5bc92faffaa5da5e228d84ae43f2de7bf611ca
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-03-01 17:06:00 +0800

    feat(web): Added bookings Page UI

---
+++ apps/web/module/Bookings/SingleView/components/Table.tsx
@@ -0,0 +1,81 @@
+"use client"
+import React, { useMemo } from "react"
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table"
+
+interface TableProps {
+  data: any[]
+  columns: any[]
+}
+
+const Table = ({ data: tableData, columns: tableColumns }: TableProps) => {
+  const data = useMemo(() => tableData, [tableData])
+  const columns = useMemo(() => tableColumns, [tableColumns])
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div>
+      <table className="w-full table-auto">
+        <thead className="text-left">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th
+                    className="pl-4"
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    scope="col"
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        <span>
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                        </span>
+                      </div>
+                    )}
+                  </th>
+                )
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr className=" hover:bg-primary-500 cursor-pointer" key={row.id}>
+                {row.getVisibleCells().map((cell, _id) => {
+                  let className = "py-2 pl-4 items-center gap-5"
+                  if (_id === 0) className += " rounded-l-xl"
+                  if (_id === row.getVisibleCells().length - 1)
+                    className += " rounded-r-xl"
+                  return (
+                    <td className={className} key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
--- apps/web/module/Bookings/index.tsx
+++ apps/web/module/Bookings/index.tsx
@@ -1,53 +1,129 @@
-"Use client"
+"use client"
 import { WidthWrapper } from "@/common/components/WidthWrapper"
-import { Typography } from "@/common/components/ui/Typography"
-import {
-  LucideChevronLeft,
-  LucideChevronRight,
-  LucidePlus,
-  LucideTable,
-} from "lucide-react"
+import Table from "./SingleView/components/Table"
 import Image from "next/image"
-import React from "react"
-import { PaymentStatus } from "./SingleView/components/ReportListing/PaymentStatus"
-import { Button } from "@/common/components/ui/Button"
+import { Typography } from "@/common/components/ui/Typography"
+import { LucidePlus, LucideTable } from "lucide-react"
+import { PaymentStatus } from "./SingleView/components/PaymentStatus"
+import { createColumnHelper } from "@tanstack/react-table"
+import Link from "next/link"
 
-const dummyData = [
-  {
-    fileKey: "/assets/1.jpg",
-    description: "Your listing started in feb 21",
-    guestCount: 5,
-    dateRange: "feb 25 - feb 29",
-    location: "Jolo, Sulu",
-    cost: "P 2,500",
-    status: "For verification",
-  },
-  {
-    fileKey: "/assets/3.jpg",
-    description: "Mahangin dito at maginhawa",
-    guestCount: 3,
-    dateRange: "feb 29 - feb 36",
-    location: "Marawi, City",
-    cost: "P 3,500",
-    status: "In progress",
-  },
-  {
-    fileKey: "/assets/3.jpg",
-    description: "Malamig tsaka mainit",
-    guestCount: 3,
-    dateRange: "feb 46 - feb 38",
-    location: "Downtown, Kinaladkad ng QC",
-    cost: "P 36,500",
-    status: "In progress",
-  },
-]
+interface ITypes {
+  fileKey: string
+  description: string
+  guestCount: string
+  dateRange: string
+  location: string
+  totalCost: string
+  paymentStatus: string
+}
 
 const Bookings = () => {
+  const data = [
+    {
+      fileKey: "3.jpg",
+      description: "Your listing started in feb 21",
+      guestCount: 5,
+      dateRange: "feb 25 - feb 29",
+      location: "Jolo, Sulu",
+      totalCost: "P 2,500",
+      paymentStatus: "For verification",
+    },
+    {
+      fileKey: "1.jpg",
+      description: "Mahangin dito at maginhawa",
+      guestCount: 3,
+      dateRange: "feb 29 - feb 36",
+      location: "Marawi, City",
+      totalCost: "P 3,500",
+      paymentStatus: "In progress",
+    },
+    {
+      fileKey: "3.jpg",
+      description: "Malamig tsaka mainit",
+      guestCount: 3,
+      dateRange: "feb 46 - feb 38",
+      location: "Downtown, Kinaladkad ng QC",
+      totalCost: "P 36,500",
+      paymentStatus: "In progress",
+    },
+    {
+      fileKey: "4.jpg",
+      description: "Malamig tsaka mainitttttt",
+      guestCount: 3,
+      dateRange: "feb 46 - feb 38",
+      location: "Downtown, Kinaladkad ng QC",
+      totalCost: "P 366,500",
+      paymentStatus: "In progress",
+    },
+    {
+      fileKey: "5.jpg",
+      description: "Malamig tsaka mainit",
+      guestCount: 3,
+      dateRange: "feb 46 - feb 38",
+      location: "Downtown, Kinaladkad ng QC",
+      totalCost: "P 36,500",
+      paymentStatus: "In progresssss",
+    },
+  ]
+  const columnHelper = createColumnHelper<ITypes>()
+  const columns = [
+    columnHelper.accessor("fileKey", {
+      header: "Listing",
+      cell: (info) => (
+        <Link href="/pornhub.com">
+          <div className="flex items-center gap-5">
+            <Image
+              className="rounded-xl"
+              src={`/assets/${info.getValue()}`}
+              alt="Image"
+              width={100}
+              height={100}
+            />
+            <span>
+              <Typography variant="p">
+                {info.row.original.description}
+              </Typography>
+            </span>
+          </div>
+        </Link>
+      ),
+    }),
+    columnHelper.accessor("guestCount", {
+      header: "Guest Count",
+      cell: (info) => <Typography variant="p">{info.getValue()}</Typography>,
+    }),
+    columnHelper.accessor("dateRange", {
+      header: "Date",
+      cell: (info) => <Typography variant="p">{info.getValue()}</Typography>,
+    }),
+    columnHelper.accessor("location", {
+      header: "Location",
+      cell: (info) => <Typography variant="p">{info.getValue()}</Typography>,
+    }),
+    columnHelper.accessor("totalCost", {
+      header: "Total Cost",
+      cell: (info) => <Typography variant="p">{info.getValue()}</Typography>,
+    }),
+    columnHelper.accessor("paymentStatus", {
+      header: "Payment Status",
+      cell: (info) => (
+        <Link href="/payment-status">
+          <div className="flex items-center">
+            <span>
+              <PaymentStatus />
+            </span>
+            <Typography variant="p">{info.getValue()}</Typography>
+          </div>
+        </Link>
+      ),
+    }),
+  ]
+
   return (
     <WidthWrapper className="mt-40 w-full">
-      {dummyData.length > 0 ? (      
       <div className="px-12">
-        <div className="mb-5">
+        <div className="mb-12">
           <Typography
             variant="h1"
             fontWeight="semibold"
@@ -64,84 +140,8 @@ const Bookings = () => {
             </div>
           </Typography>
         </div>
-        <div>
-          <table className="w-full table-auto">
-            <thead className="text-left">
-              <tr>
-                <th className="pl-4">Listing</th>
-                <th>Guest</th>
-                <th>Date</th>
-                <th>Location</th>
-                <th>Cost</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dummyData.map((data) => (
-                <tr
-                  className="hover:bg-primary-500 cursor-pointer"
-                  key={data.fileKey}
-                >
-                  <td className="py-4 pl-4 rounded-tl-xl rounded-bl-xl items-center gap-5">
-                    {
-                      <div className="flex items-center gap-5">
-                        <Image
-                          className="rounded-xl"
-                          width={100}
-                          height={100}
-                          src={data.fileKey}
-                          alt="Image"
-                        />
-                        <span className="">{data.description}</span>
-                      </div>
-                    }
-                  </td>
-                  <td>{data.guestCount}</td>
-                  <td>{data.dateRange}</td>
-                  <td>{data.location}</td>
-                  <td>{data.cost}</td>
-                  <td className="py-4 pr-4 rounded-tr-xl rounded-br-xl">
-                    <PaymentStatus />
-                    {data.status}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <Table data={data} columns={columns} />
       </div>
-      ) : (
-      <div className="px-12">
-        <Typography variant="h1" fontWeight="semibold">
-          Trippings
-        </Typography>
-        <hr className="mt-5 mb-5"></hr>
-        <Typography variant="h1" fontWeight="semibold">
-          No trips booked...yet!
-        </Typography>
-        <Typography
-          variant="h4"
-          fontWeight="semibold"
-          className="text-gray-600"
-        >
-          Time to dust off your bags and start planning your next adventure
-        </Typography>
-        <Button variant="outline" size="lg" className="mt-3 font-semibold">
-          Start searching
-        </Button>
-        <hr className="mt-12 mb-5"></hr>
-        <Typography
-          variant="h4"
-          fontWeight="semibold"
-          className="text-gray-600"
-        >
-          Can't find your reservation here?{" "}
-          <button className="font-semibold underline text-gray-800">
-            Visit the help center
-          </button>
-        </Typography>
-      </div>
-      )}
     </WidthWrapper>
   )
 }
