cd605cf32259904ff41ba4c5d49e831d1bb05524
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-01 09:03:19 +0800

    feat(web): wip Reusable component for notification

--- apps/web/module/Notification/components/Notification.tsx
+++ apps/web/module/Notification/components/Notification.tsx
@@ -2,11 +2,17 @@ import { Typography } from "@/common/components/ui/Typography";
 import { cn } from "@/common/helpers/cn";
 import React from "react"
 
-interface NotificationContent {
-    avatarKey: string;
-    user: string;
-    description: string;
-    time: string;
+export interface NotificationContent {
+    id:number,
+    profilePicture: string;
+    name: string;
+    listing:{
+        hostId:number,
+        title:string
+    };
+    type:string,
+    days?:number,
+    createdAt: string;
 
   }
   interface NotificationProps {
@@ -15,15 +21,21 @@ interface NotificationContent {
   }
 
   const Notifications =({content,size}:NotificationProps)=>{
-
+   
     return <div className="notification-container ">
         <div>
             <Typography variant="h2" fontWeight="semibold" className="mb-4">
                 Activity
             </Typography>
         </div>
-          {content?.map((item) => (
-              <div key={item.description} className="notification ">
+          {content?.map((item:NotificationContent) => {
+             const now = new Date()
+             const createdAt = new Date(item.createdAt)
+             const timeDifferenceInMillis = Number(now) - Number(createdAt)
+             const timeDifferenceInHours = timeDifferenceInMillis / (1000 * 60 * 60);
+             const timeDifferenceInDays = timeDifferenceInHours / 24;
+            return(
+              <div key={item.id} className="notification ">
                   <div className="content">
                 <div className="flex items-start right-0 border-b">
                     <div className="py-4 pr-2">
@@ -34,7 +46,7 @@ interface NotificationContent {
                         )}
                     >
                         <img
-                        src={`/assets/${item.avatarKey}`}
+                        src={`/assets/${item.profilePicture}`}
                         width={10}
                         height={10}
                         alt="Avatar"
@@ -45,21 +57,22 @@ interface NotificationContent {
                     </div>
                     <div className="py-4 pl-2">
                          <Typography variant="p" fontWeight="semibold">
-                         {item.user}
+                         {item.name}
                         </Typography>
-                        <Typography variant="p">
-                        {item.description}
-                        </Typography>
+                       {item.type==="Booking" ? (<p>
+                        <span>{item.name}</span> booked your listing {item.listing.title} for {item.days} days
+                       </p>):(<p><span>{item.name}</span> added a review to your listing {item.listing.title}</p>)}
                     </div>
                     <div className="py-4 pl-2 justify-self-end">
                         <Typography variant="p">
-                         {item.time}
+                         {timeDifferenceInHours>=24 ?timeDifferenceInDays.toFixed(0)+"d":timeDifferenceInHours.toFixed(0)+"h"}
                         </Typography>
                     </div>
                 </div>
                   </div>
               </div>
-          ))}
+            
+          )})}
       </div>;
 }
   export default Notifications;
--- apps/web/module/Notification/index.tsx
+++ apps/web/module/Notification/index.tsx
@@ -1,12 +1,13 @@
+"use client"
 import useGetNotifications from "./hooks/useGetNotifications"
-import Notifications from "./components/Notification"
+import Notifications, { NotificationContent } from "./components/Notification"
 const NotificationUI = ()=>{
 
     const {data, isPending} = useGetNotifications(1)
     return(
         <div>
         {!isPending &&(
-            <Notifications content={data?.items} />
+            <Notifications content={data?.items as NotificationContent[]} />
         )}
         </div>
     )
