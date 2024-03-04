f4a175c805e99b51c9a2f92c788831409c5261ce
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-04 16:25:55 +0800

    feat(api): Updated profile endpoint and hook

--- apps/api/src/routes/users/service/userProfile.ts
+++ apps/api/src/routes/users/service/userProfile.ts
@@ -1,12 +1,11 @@
-import {
-  REQUIRED_VALUE_EMPTY,
-  UNKNOWN_ERROR_OCCURRED,
-  USER_NOT_EXIST,
-} from '@/common/constants'
-import { prisma } from '@/common/helpers/prismaClient'
-import { FileService } from '@/common/service/file'
+// import {
+//   REQUIRED_VALUE_EMPTY,
+//   UNKNOWN_ERROR_OCCURRED,
+//   USER_NOT_EXIST,
+// } from '@/common/constants'
+// import { prisma } from '@/common/helpers/prismaClient'
+// import { FileService } from '@/common/service/file'
 import { ResponseService } from '@/common/service/response'
-import { File } from 'buffer'
 import { Request, Response } from 'express'
 let profiles = [
   {
