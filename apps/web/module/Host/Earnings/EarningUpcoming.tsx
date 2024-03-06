b7c71762d5c21ff59f628b1015d678c16c7615d5
Andal <arjayandal93@gmail.com>
2024-03-06 17:27:58 +0800

    feat(web): Setup Earning upcoming UI

--- apps/web/module/Host/Earnings/EarningUpcoming.tsx
+++ apps/web/module/Host/Earnings/EarningUpcoming.tsx
@@ -34,14 +34,16 @@ const EarningUpcoming = () => {
         <>
           <div className="left p-4 lg:col-span-3">
             {data?.item && data.item.amount.length > 0 ? (
-              <Chart
-                data={data.item.amount}
-                totalAmount={data.item.total}
-                isPending={isPending}
-                width="100%"
-                height={400}
-                type={ChartType.upcoming}
-              />
+              
+                <Chart
+                  data={data.item.amount}
+                  totalAmount={data.item.total}
+                  isPending={isPending}
+                  width="100%"
+                  height={400}
+                  type={ChartType.upcoming}
+                />
+              
             ) : (
               <>
                 <Typography fontWeight="semibold" variant="h2" className="pb-4">
@@ -64,18 +66,15 @@ const EarningUpcoming = () => {
                 </Typography>
 
                 <div className="flex gap-4 justify-between pb-4">
-                  {summaryData.map((column, id) => (
-                    <div key={`column-${id}`} className="flex flex-col">
-                      {column.map((item, itemIndex) => (
-                        <h6
-                          key={`column-${id}-item-${itemIndex}`}
-                          className="pt-2 text-sm"
-                        >
-                          {item}
-                        </h6>
-                      ))}
-                    </div>
-                  ))}
+                {summaryData.map((column, columnIndex) => (
+                  <div key={`column-${columnIndex}`} className="flex flex-col">
+                    {column.map((item) => (
+                      <h6 key={item} className="pt-2 text-sm">
+                        {item}
+                      </h6>
+                    ))}
+                  </div>
+                ))}
                 </div>
 
                 <div className="bottom-0 border-t flex gap-4 justify-between">
