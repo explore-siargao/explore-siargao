83266de685eb94c95697371404809834e624846a
Andal <arjayandal93@gmail.com>
2024-03-07 08:27:46 +0800

    feat(web): Setup Earning upcoming UI

--- apps/web/module/LandingPage/index.tsx
+++ apps/web/module/LandingPage/index.tsx
@@ -5,7 +5,6 @@ import useGetAllBookings from "../LandingPage/hooks/useGetAllBookings"
 import { Spinner } from "@/common/components/ui/Spinner"
 import useSessionStore from "@/common/store/useSessionStore"
 import Listing from "../Listing"
-import EarningUpcoming from "../Host/Earnings/EarningUpcoming"
 
 const LandingPage = () => {
   const userId = useSessionStore((state) => state).id
@@ -37,7 +36,6 @@ const LandingPage = () => {
           </ul>
         </>
       )}
-      <EarningUpcoming/>
     </WidthWrapper>
   )
 }
