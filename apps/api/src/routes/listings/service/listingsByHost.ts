07fa0fd70ea8b9a3ffce7e63ceb9585d15948cc0
ramil25 <ramil25@users.noreply.github.com>
2024-03-07 07:32:32 +0000

    Prettified Code!

--- apps/api/src/routes/listings/service/listingsByHost.ts
+++ apps/api/src/routes/listings/service/listingsByHost.ts
@@ -5,7 +5,7 @@ import { ResponseService } from '@/common/service/response'
 const response = new ResponseService()
 export const getListingsByHost = async (req: Request, res: Response) => {
   const hostId = Number(res.locals.user.id)
-  const filterListings =  listingsByHost.filter((listing) => {
+  const filterListings = listingsByHost.filter((listing) => {
     return listing.hostId === hostId
   })
   res.json(
