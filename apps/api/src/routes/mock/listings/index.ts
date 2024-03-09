b5b527a3f0fa93d36bd0bb3cd2ee047fcc2a70c2
jpmadrigal07 <jpmadrigal07@users.noreply.github.com>
2024-03-09 01:35:12 +0000

    Prettified Code!

--- apps/api/src/routes/mock/listings/index.ts
+++ apps/api/src/routes/mock/listings/index.ts
@@ -1,14 +1,10 @@
-
 import express from 'express'
 import { getListingsByHost } from './service/listingsByHost'
 
 const router = express.Router()
 
 // DEFAULT
-router.get(
-  '/hosted',
-  getListingsByHost
-)
+router.get('/hosted', getListingsByHost)
 
 export default router
 
