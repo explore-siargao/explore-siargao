cc0a24b920f5b533095f7b5357bac367c4a7596e
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-03-11 17:16:28 +0800

    feat(web) Added daily graph for month

---
+++ apps/web/app/(host)/host/earnings/paid/[month-year]/page.tsx
@@ -0,0 +1,11 @@
+import Daily from "@/module/Host/Earnings/DailyEarnings"
+
+const PaidSection = () => {
+    return (
+        <div className="mt-40 p-8">
+            <Daily/>
+        </div>
+    
+    )
+}
+export default PaidSection
