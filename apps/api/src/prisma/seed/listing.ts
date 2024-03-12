0807329f257c777fd25c62d6e7529fff4b5c86aa
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-12 09:35:47 +0800

    feat(api):Maked dummy data in seeder formal

--- apps/api/src/prisma/seed/listing.ts
+++ apps/api/src/prisma/seed/listing.ts
@@ -13,6 +13,12 @@ export const listing = async () => {
     },
   })
 
+  const getBasicAboutPlace = await prisma.basicAboutPlace.findMany({
+    where:{
+      deletedAt:null
+    }
+  })
+
   const createListings = await prisma.listing.createMany({
     data: [
       {
@@ -40,22 +46,23 @@ export const listing = async () => {
             alt: 'image',
           },
         ]),
-        title: 'Test',
+        title: 'Casa del Mar',
         category: 'Accomodation',
-        address: 'Santa Maria, Laguna',
+        address: 'Entire home in Mabini, Philippines',
         latitude: '9.968644911072126',
         longitude: '126.0799420362242',
         whereYoullBe: JSON.stringify({
-          description: 'Long description',
-          workAround: 'Work around information',
+          description: 'Mabini, Calabarzon, Philippines',
+          workAround: '',
         }),
         whereYoullSleep: JSON.stringify({
-          kingBed: {
-            count: 1,
-            size: 'Large',
+          bedroom: {
+            count: 2,
+            size: 'Double bed',
             icon: 'bed',
           },
         }),
+        basicAboutPlaceId:getBasicAboutPlace[0]?.id || 0
       },
       {
         hostedById: getUsers[1]?.id || 0,
@@ -82,22 +89,23 @@ export const listing = async () => {
             alt: 'image',
           },
         ]),
-        title: 'hello World',
+        title: 'Arcadia private resort -beach front property',
         category: 'Accomodation',
-        address: 'Santa Maria, Laguna',
-        latitude: '9.784645441809563',
-        longitude: '126.04560975969396',
+        address: 'Island in Ligpo Island, Philippines',
+        latitude: '9.7813',
+        longitude: '126.1181',
         whereYoullBe: JSON.stringify({
-          description: 'Long description',
-          workAround: 'Work around information',
+          description: 'Ligpo Island, Batangas, Philippines It is very quite, (if we are honest maybe a boat now again) normally we are just listening to birds/wild life and the gentle crash of waves against the shore/beach.',
+          workAround: '',
         }),
         whereYoullSleep: JSON.stringify({
-          kingBed: {
-            count: 1,
+          QueenBed: {
+            count: 3,
             size: 'Large',
             icon: 'bed',
           },
         }),
+        basicAboutPlaceId:getBasicAboutPlace[1]?.id || 0
       },
       {
         hostedById: getUsers[1]?.id || 0,
@@ -124,14 +132,14 @@ export const listing = async () => {
             alt: 'image',
           },
         ]),
-        title: 'Amazing World',
+        title: 'Relaxing Sea Breeze Suite at Pico de Loro',
         category: 'Accomodation',
-        address: 'Siniloan, Laguna',
-        latitude: '9.919949195329322',
-        longitude: '125.993424699368',
+        address: 'Entire condo in Nasugbu, Philippines',
+        latitude: '9.8592',
+        longitude: '126.0623',
         whereYoullBe: JSON.stringify({
-          description: 'Long description',
-          workAround: 'Work around information',
+          description: 'Nasugbu, Calabarzon, Philippines Pico De Loro is a private beach residential resort located inside Hamilo Coast, in the municipality of Nasugbu, a scenic coastal town in the province of Batangas.',
+          workAround: 'Getting to Pico de Loro requires a private vehicle as there are no available public transportation nearby. Taking CAVITEX is the fastest way to get to the place. Please drive carefully towards the place as there are a lot of curved roads and it`s not well-lighted at night.',
         }),
         whereYoullSleep: JSON.stringify({
           kingBed: {
@@ -139,7 +147,18 @@ export const listing = async () => {
             size: 'Large',
             icon: 'bed',
           },
+          queenBed: {
+            count: 1,
+            size: 'Large',
+            icon: 'bed',
+          },
+          singleBed: {
+            count: 1,
+            size: 'Small',
+            icon: 'bed',
+          },
         }),
+        basicAboutPlaceId:getBasicAboutPlace[2]?.id || 0 
       },
       {
         hostedById: getUsers[0]?.id || 0,
@@ -166,22 +185,23 @@ export const listing = async () => {
             alt: 'image',
           },
         ]),
-        title: 'Zkript View',
+        title: 'Simala Beach House',
         category: 'Accomodation',
-        address: 'Paete, Laguna',
-        latitude: '10.046407690620544',
-        longitude: '126.05865602477546',
+        address: 'Entire home in Sibonga, Philippines',
+        latitude: '9.7817',
+        longitude: '126.1212',
         whereYoullBe: JSON.stringify({
-          description: 'Long description',
-          workAround: 'Work around information',
+          description: "Sibonga, Central Visayas, Philippines The house is conveniently located in close proximity to various attractions and amenities. Seargao Seaview Cafe is just about 5.5 kilometers away, offering a great dining option. Additionally, the famous Miraculous Shrine of Mary in Lindogon, Simala, Cebu, is only 3.4 kilometers away, providing a place for spiritual reflection and visits. For everyday needs, there is a Public Market along the main road, along with bakeries, a 711 convenience store, and a laundromat, ensuring that you have easy access to essential items and services. The main town of Sibonga is approximately 6.3 kilometers away, where you can find further amenities and facilities. If you are looking for a wider range of shopping options and supermarkets, Carcar is located about 18 kilometers from the house. Furthermore, If you wish to visit the Archbishop Teofilo Camomot Shrine in Carcar, it is approximately 19 kilometers away, providing a spiritual site for reflection and devotion. The shrine offers a serene and sacred atmosphere, allowing visitors to engage in prayer and contemplation.",
+          workAround: "The house's convenient location within walking distance to main road ensures easy access to various modes of public transportation. Whether you prefer buses, tricycles, jeepneys, or motorcycles for hire, you will have plenty of options available to explore the surrounding areas and conveniently travel to different destinations. This accessibility makes it convenient for you to commute and navigate the area with ease.",
         }),
         whereYoullSleep: JSON.stringify({
           kingBed: {
-            count: 1,
+            count: 3,
             size: 'Large',
             icon: 'bed',
           },
         }),
+        basicAboutPlaceId:getBasicAboutPlace[3]?.id || 0 
       },
       {
         hostedById: getUsers[2]?.id || 0,
@@ -208,14 +228,14 @@ export const listing = async () => {
             alt: 'image',
           },
         ]),
-        title: 'Test',
+        title: 'Amihan Coron -Havement',
         category: 'Accomodation',
-        address: 'Santa Maria, Laguna',
-        latitude: '9.91047974222796',
-        longitude: '125.94673280328686',
+        address: 'Entire rental unit in Coron, Philippines',
+        latitude: '9.7533',
+        longitude: '126.1586',
         whereYoullBe: JSON.stringify({
-          description: 'Long description',
-          workAround: 'Work around information',
+          description: 'Coron, MIMAROPA, Philippines',
+          workAround: '',
         }),
         whereYoullSleep: JSON.stringify({
           kingBed: {
@@ -224,6 +244,7 @@ export const listing = async () => {
             icon: 'bed',
           },
         }),
+        basicAboutPlaceId:getBasicAboutPlace[4]?.id || 0 
       },
     ],
   })
--- apps/api/src/prisma/seed/listingDescription.ts
+++ apps/api/src/prisma/seed/listingDescription.ts
@@ -9,40 +9,39 @@ export const listingDescription = async () => {
   const createListingDescriptions = await prisma.listingDescription.createMany({
     data: [
       {
-        generalDescription: 'general description',
-        aboutGuestAccess: 'about guest access',
-        aboutSpace: 'About space',
+        generalDescription: 'This home has peace of mind, relax with your whole family!',
+        aboutGuestAccess: '',
+        aboutSpace: '',
         listingId: getListings[0]?.id,
       },
       {
         generalDescription:
-          'Open the complimentary wine and listen to music via retro Marshall speakers. Here custom wood furniture meets textured concrete walls, plush Persian carpets, classic vintage pieces and 60s pop art accents. A refined fusion of industrial and retro features ultimately lends this loft its unique, special character. Perfect for a photogenic boutique art hotel vibe.',
-        otherThingsNote:
-          'CHECK IN REQUIREMENTS **- Govt ID (passport, drivers license etc)- Covid vaccination card- Required for all adults and children',
+          'Private Paradise, Beach Front We can accomodate 2-12pax split over the 3 cottage style bedrooms.Additional bedding can be provided (no additional cost) to a maximum capacity of 2-4 a room if guest are willing to share bed/matress Note: For larger groups above 12pax we have a additional cottage rooms available, and pay upon arrival.. Transfer boat from car park to the beach is free of charge We have snorkeling equipment and booties for rent at the property 100pesos only Best wishes:)',
+          aboutGuestAccess:'Guest have full access to the property, and use of the facility`s,',
+          otherThingsNote:'Take note this time of pandemic we required every guest to have a medical certificate for our all safety requirements Thank you',
         aboutSpace:
-          'ALTO RETRO is designed and conceptualized by the owner, all inspired from New York City loft apartments. Highly sought after with features in Spotph, DiscoverMNL, Real Living Magazine and ClicktheCitycom.',
+          'Private Cottage bedrooms, situated on a private ocean front with green areas shaded by trees and palms. Great place to relax by the pool or with cushions on the lawn... Beach is sand/coral, more towards coral great for snorkelling, diving and swimming. Swim shoes are a must, and we have at the place and you can rent it for only 100pesos Great place to get away from the busy city.. set and relax Note: if you need a chef for hustle free pls let us know thank you :)',
         listingId: getListings[1]?.id,
       },
       {
         generalDescription:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Relax and unwind in our spacious and vibrant 55 sqm unit at Pico de Loro Beach & Country Club. Inspired by the nearby seascape, you will have a memorable vacation inside our spacious home. Pico de Loro is an exclusive beach club that requires membership to have access. Entrance fee is not yet included in the price. Unit is located at Jacana B.",
         aboutGuestAccess:
-          "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        aboutSpace:
-          'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum',
-        otherThingsNote: 'other things need to know',
+          "IMPORTANT! ADDITIONAL GUEST FEE The payment in Airbnb only covers the rental of the condominium unit. All guests must pay the entrance fee at the Guest Registration Area / Country Club Reception as per management rules. This is good for 7 consecutive days maximum. The wrist band will be replaced every day at the concierge of Beach Club or Country Club. Upon payment of the entrance fee, you may now be able to access the facilities and amenities of the resort.",
+        aboutSpace:"This condominium unit has an open layout that makes it a spacious furnished unit with 1 king-sized, 1 queen-sized and 1 single beds for your comfortable stay. It is fully equipped with A/C, comfortable bed sheets and pillows. Feel free to store your personal stuff inside the built-in cabinets during your stay. The unit has a veranda with table and chairs where you can enjoy the mountain view and breath fresh air while socializing with your loved ones.",        
+        otherThingsNote: 'Our ExploreSiargao can accommodate up to 6 people only. Any guests not accounted for in the initial booking shall not be permitted to stay.',
         listingId: getListings[2]?.id,
       },
       {
-        generalDescription:
-          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+        generalDescription:"Escape to our beachfront oasis in Sibonga. Experience stunning ocean views, exclusive privacy, and comfortable accommodations. Book your stay now and enjoy the serenity of beachfront bliss.",
+        aboutSpace:"Experience the ultimate beachfront getaway with friends and family in Sibonga. Our fenced beachfront house ensures the safety of our guests while providing a serene and memorable vacation. Here's why our property is the perfect choice for your retreat: 1. Secure: Our house offers ample parking space for at least four vehicles and is fenced all around to ensure the safety and privacy of our guests. 2. Bahay Kubo and Oceanfront Scenery: Unwind in our Bahay Kubo or tiki hut, an ideal spot for watching the sunrise, enjoying a morning coffee, or simply immersing yourself in the peaceful surroundings. The separate kitchenette allows you to indulge in meals while gazing through the kitchen window at the mesmerizing oceanfront scenery. 3. Master Bedroom Retreat: The master bedroom features a private bathroom with a hot and cold shower, providing utmost comfort and convenience. The highlight is the spacious terrace overlooking the ocean, where you can relax, enjoy the view, and stargaze at night, creating unforgettable memories with friends and family. 4. Comfortable and Air-Conditioned: All bedrooms in the house are air-conditioned, ensuring a comfortable stay for everyone. The 2nd and 3rd bedrooms share a bathroom, offering convenience for guests who stay in those rooms. 5. Welcoming Layout: The house features a well-designed layout that promotes a warm and inviting atmosphere. On the main floor, you'll find a bathroom and shower, providing additional convenience. The kitchen, dining area, and living room are thoughtfully arranged, allowing for easy interaction and connection between guests. 6. Convenient Location: Our property is conveniently situated within walking distance from the main road, providing easy access to public transportation. The nearby local public market offers fresh produce, while a 711 Store and Laundromat take care of your everyday needs. Discover the joy of a beachfront getaway that combines relaxation, mesmerizing oceanfront scenery, and a convenient location in Sibonga. Book your stay with us now for an unforgettable vacation.",
+        aboutGuestAccess:"Entire house",
+        otherThingsNote:"The house is situated in close proximity to the Cockpit Arena. On Sundays and holidays, there are scheduled cockfighting events taking place at the arena, which can lead to some noise disturbances between the hours of noon and 5 pm.",
         listingId: getListings[3]?.id,
       },
       {
-        generalDescription:
-          "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-        aboutGuestAccess:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        generalDescription:'Welcome to Amihan Coron! Charming units at the heart of Coron Town where you can vibe with your friends, family and nature. Take photos, make lasting memories with us as you explore the world famous "Enchanting Coron"',
+        aboutGuestAccess:'',
         listingId: getListings[4]?.id,
       },
     ],
