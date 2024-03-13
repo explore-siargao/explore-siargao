cc0b9f5cb2bd4e1590aa40a713d2cf6f6aba0612
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-13 16:10:53 +0800

    fix(api): Fixed mock data and move it to mock url

--- apps/api/src/routes/mock/bookings/index.ts
+++ apps/api/src/routes/mock/bookings/index.ts
@@ -5,6 +5,10 @@ import { getThisMonthEarnings } from './services/thisMonth'
 import { getUpcomingEarnings } from './services/upcoming'
 import { getPaidEarnings } from './services/paid'
 import { getMonthYearEarnings } from './services/monthYear'
+import { getThisMonthBookings } from './services/thisMonthBooking'
+import { getPaidBookings } from './services/paidBooking'
+import { getUpcomingBookings } from './services/upcomingBooking'
+import { getMonthYearBookings } from './services/monthYearBooking'
 
 const router = express.Router()
 
@@ -15,8 +19,12 @@ router.get('/paginated', paginatedBooking)
 
 //eranings
 router.get('/earnings/this-month', getThisMonthEarnings)
+router.get('/earnings/this-month/bookings', getThisMonthBookings)
 router.get('/earnings/upcoming', getUpcomingEarnings)
+router.get('/earnings/upcoming/bookings', getUpcomingBookings)
 router.get('/earnings/paid', getPaidEarnings)
-router.get('/earnings/month-year', getMonthYearEarnings)
+router.get('/earnings/paid/bookings', getPaidBookings)
+router.get('/earnings/:monthYear', getMonthYearEarnings)
+router.get('/earnings/:monthYear/bookings', getMonthYearBookings)
 
 export default router
