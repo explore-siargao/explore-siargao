742ad8a910283698f64b6bc30926e32ea0e559c1
alkejah <alkejah@users.noreply.github.com>
2024-03-15 09:29:32 +0000

    Prettified Code!

--- apps/web/module/Host/Earnings/MonthlyEarningTable.tsx
+++ apps/web/module/Host/Earnings/MonthlyEarningTable.tsx
@@ -3,14 +3,15 @@ import Table from "@/common/components/Table"
 import useGetMonthYearEarningsWithBookings from "../hooks/useGetMonthYearEarningsWithBookings"
 import { ColumnsTab } from "../constants/columns"
 
-
 const MonthlyEarningTable = ({ date }: { date: string }) => {
   const { data: bookings, isPending } =
     useGetMonthYearEarningsWithBookings(date)
-    const filteredBookings = bookings?.item?.bookings?.filter((booking: PaymentHistoryBookingsData) => {
-        return booking.dateFrom !== null
-      })
-      
+  const filteredBookings = bookings?.item?.bookings?.filter(
+    (booking: PaymentHistoryBookingsData) => {
+      return booking.dateFrom !== null
+    }
+  )
+
   const columns = ColumnsTab
   return (
     <div className="pt-8">
