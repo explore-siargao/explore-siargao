9470d71405d12ee95f31894765ca94d506489d73
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-03-15 17:26:55 +0800

    feat(web): Added column for earning month table

--- apps/web/module/Host/Earnings/MonthlyEarningTable.tsx
+++ apps/web/module/Host/Earnings/MonthlyEarningTable.tsx
@@ -1,16 +1,16 @@
 import { PaymentHistoryBookingsData } from "@/common/components/Table/Type"
 import Table from "@/common/components/Table"
-import { ColumnsTab } from "../constants/columns"
 import useGetMonthYearEarningsWithBookings from "../hooks/useGetMonthYearEarningsWithBookings"
+import { ColumnsTab } from "../constants/columns"
+
 
 const MonthlyEarningTable = ({ date }: { date: string }) => {
   const { data: bookings, isPending } =
     useGetMonthYearEarningsWithBookings(date)
-  const filteredBookings =
-    bookings &&
-    bookings?.item?.bookings?.filter((booking: PaymentHistoryBookingsData) => {
-      return booking.dateFrom !== null
-    })
+    const filteredBookings = bookings?.item?.bookings?.filter((booking: PaymentHistoryBookingsData) => {
+        return booking.dateFrom !== null
+      })
+      
   const columns = ColumnsTab
   return (
     <div className="pt-8">
