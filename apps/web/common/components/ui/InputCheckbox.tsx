f23871ab9b03f630e9bffb18ddf8228a0c9177f8
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-03-19 06:57:25 +0000

    Prettified Code!

--- apps/web/common/components/ui/InputCheckbox.tsx
+++ apps/web/common/components/ui/InputCheckbox.tsx
@@ -1,26 +1,25 @@
-
 interface CheckboxProps {
-  id: number;
-  colorVariant: string;
-  checked: boolean;
-  onChange: (id: number, event: React.ChangeEvent<HTMLInputElement>) => void;
+  id: number
+  colorVariant: string
+  checked: boolean
+  onChange: (id: number, event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 const Checkbox = ({ id, colorVariant, checked, onChange }: CheckboxProps) => {
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(id, e);
-  };
+    onChange(id, e)
+  }
 
   const getColorClassName = () => {
     switch (colorVariant) {
       case "primary":
-        return "text-primary-500 focus:ring-primary-500";
+        return "text-primary-500 focus:ring-primary-500"
       case "secondary":
-        return "text-secondary-500 focus:ring-secondary-500";
+        return "text-secondary-500 focus:ring-secondary-500"
       default:
-        return "";
+        return ""
     }
-  };
+  }
 
   return (
     <div>
@@ -32,7 +31,7 @@ const Checkbox = ({ id, colorVariant, checked, onChange }: CheckboxProps) => {
         className={`form-checkbox ${getColorClassName()} h-4 w-4 rounded`}
       />
     </div>
-  );
-};
+  )
+}
 
-export default Checkbox;
+export default Checkbox
