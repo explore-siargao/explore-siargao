7e34cc58fda6d36bd440316f966389907b630c5b
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-19 16:42:13 +0800

    fat(api): Fixed seeder personalInfo Change to Guest

--- apps/api/src/prisma/seed/guest.ts
+++ apps/api/src/prisma/seed/guest.ts
@@ -1,23 +1,25 @@
 import { prisma } from '@/common/helpers/prismaClient'
 
-export const personalInfo = async () => {
+export const guest = async () => {
   const getUsers = await prisma.user.findMany({
     where: {
       deletedAt: null,
     },
   })
 
-  const createPersonalInfos = await prisma.personalInfo.createMany({
+  const createPersonalInfos = await prisma.guests.createMany({
     data: [
       {
         firstName: 'test',
         lastName: 'account',
         birthDate: '2014-02-19T12:34:56.789Z',
         userId: getUsers[0]?.id ? getUsers[0].id : 0,
-        phoneNumber: '09092558726',
+        phone: '555-7777',
+        cellPhone:'09092558726',
         country: 'PH',
         currency: 'PHP',
         language: 'English',
+        gender:'M',
         confirm: JSON.stringify({
           identity: true,
           emailAddress: true,
@@ -29,10 +31,12 @@ export const personalInfo = async () => {
         lastName: 'Kaharian',
         birthDate: '2014-02-19T12:34:56.789Z',
         userId: getUsers[1]?.id ? getUsers[1].id : 0,
-        phoneNumber: '09092558726',
+        phone: '554-222',
+        cellPhone:'09092558726',
         country: 'PH',
         currency: 'PHP',
         language: 'English',
+        gender:'M',
         confirm: JSON.stringify({
           identity: true,
           emailAddress: true,
@@ -44,7 +48,9 @@ export const personalInfo = async () => {
         lastName: 'Madrigal',
         birthDate: '2014-02-19T12:34:56.789Z',
         userId: getUsers[2]?.id ? getUsers[2].id : 0,
-        phoneNumber: '09092558726',
+        phone:'',
+        cellPhone: '09092558726',
+        gender:'M',
         country: 'PH',
         currency: 'PHP',
         language: 'English',
@@ -59,7 +65,9 @@ export const personalInfo = async () => {
         lastName: 'Dela pena',
         birthDate: '2014-02-19T12:34:56.789Z',
         userId: getUsers[3]?.id ? getUsers[3].id : 0,
-        phoneNumber: '09092558726',
+        phone:'',
+        cellPhone: '09092558726',
+        gender:'M',
         country: 'PH',
         currency: 'PHP',
         language: 'English',
@@ -74,7 +82,9 @@ export const personalInfo = async () => {
         lastName: 'Andal',
         birthDate: '2014-02-19T12:34:56.789Z',
         userId: getUsers[4]?.id ? getUsers[4].id : 0,
-        phoneNumber: '09092558726',
+        phone:'554-757',
+        cellPhone: '09092558726',
+        gender:'M',
         country: 'PH',
         currency: 'PHP',
         language: 'English',
