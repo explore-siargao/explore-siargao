99c979a335ab74762c9cb612a3ad5f3ae9f39bbd
kinnethjavier <kinnethandrew9@gmail.com>
2024-03-19 15:38:10 +0800

    wip(api): Working on mock reservations api

---
+++ apps/api/src/routes/mock/reservations/jsons/reservations.ts
@@ -0,0 +1,15 @@
+export const reservations = [
+    {
+        id: 1,
+        propertyId: 1,
+        mainGuestId: 1,
+        status: "not_confirmed",
+        startDate: "2024-03-19 03:09:52.697",
+        endDate: "2024-03-20 03:09:52.697",
+        guestList: [
+            {
+                id: 2,
+            }
+        ]
+    }
+]
