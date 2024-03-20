1413f5a642382257e07280d6838bdb89cb4056a9
Dyayy026 <Dyayy026@users.noreply.github.com>
2024-03-20 01:26:27 +0000

    Prettified Code!

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   extends: ["@repo/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["d:/documents/zcript/exploresiargao/explore-siargao/tsconfig.json"],
+    project: [
+      "d:/documents/zcript/exploresiargao/explore-siargao/tsconfig.json",
+    ],
   },
 }
