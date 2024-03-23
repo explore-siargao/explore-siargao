97f7b97f98341b9d5505b7f4c86c1ada8958bd7d
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-03-23 17:02:06 +0800

    fix: Remove /api in users auth

--- apps/web/module/Authentication/constants/api-routes.ts
+++ apps/web/module/Authentication/constants/api-routes.ts
@@ -1,4 +1,4 @@
-export const API_USERS_AUTH = "/api/users/auth"
+export const API_USERS_AUTH = "/users/auth"
 
 export const API_AUTH_FORGOT_PASSWORD = `${API_USERS_AUTH}/forgot-password`
 export const API_AUTH_FORGOT_PASSWORD_VERIFY = `${API_USERS_AUTH}/forgot-password/verify`
