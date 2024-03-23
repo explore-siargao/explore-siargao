8b1358e5627660fb408dd6ed14862028c429c1f0
John Patrick Madrigal <jp.madrigal07@gmail.com>
2024-03-23 16:20:37 +0800

    ci: Fix vulnerabilities on packages and env vars

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import("next").NextConfig} */
-const { existsSync } = require("fs")
 require("dotenv").config({
-  path: existsSync("../../.env") ? "../../.env" : "../../../.env",
+  path: "../../.env",
 })
 
 module.exports = {
