03e794ebfa94ab0a8fb27eb7a58f4b66e64826da
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-03-25 14:56:56 +0800

    Created 404 page

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,14 +1,15 @@
+import React from "react"
+import { Typography } from "@/common/components/ui/Typography"
+import Link from "next/link"
 
-import { Typography } from "@/common/components/ui/Typography";
-import Link from "next/link";
+const NotFound = () => {
+  return (
+        <div className="w-full h-screen mx-auto text-center">
+        <Typography variant="h1">404 | not found</Typography>
+          <Link href="/">Go back</Link>
+        </div>
 
-export default function NotFound() {
-    return (
-            <div>
-                <Typography variant="h1">Oops...</Typography>
-                <Typography variant="p">Page not found!</Typography>
-                <Link href="/">Go back</Link>
-            </div>
-    )
+  )
 }
 
+export default NotFound
