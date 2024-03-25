4056cc4dfa9fa1fb2181867cf2def07f9816b6e2
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-03-25 15:38:47 +0800

    Created 404 page

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,17 +1,34 @@
 import React from "react"
 import { Typography } from "@/common/components/ui/Typography"
 import Link from "next/link"
+import { Button } from "@/common/components/ui/Button"
 
 const NotFound = () => {
   return (
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center", height:"95vh", overflow:"hidden"  }}>
-        <div  style={{ textAlign:"center", overflow:"hidden"}}>
-          <Typography variant="h1">404 - Page Not Found</Typography>
-          <Typography>Sorry, the page you're looking for does not exist.</Typography>
-        </div>
-        </div>
-      );
-
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "95vh",
+        overflow: "hidden",
+      }}
+    >
+      <div style={{ textAlign: "center", overflow: "hidden" }}>
+        <Typography variant="h1" fontWeight="semibold">
+          404 - Page Not Found
+        </Typography>
+        <Typography>
+          Sorry, the page you're looking for does not exist.
+        </Typography>
+        <Link href="/">
+          <Button variant="default" className="mt-2">
+            Go back
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound
