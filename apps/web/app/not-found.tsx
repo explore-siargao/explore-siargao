4c98f9ccc50d02319774f6936ec5e1b1e19cc139
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-25 13:35:32 +0800

    Created 404 page

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,14 +1,14 @@
-import LayoutWrapper from "@/common/components/LayoutWrapper";
+
 import { Typography } from "@/common/components/ui/Typography";
 import Link from "next/link";
 
 export default function NotFound() {
     return (
-            <LayoutWrapper>
+            <div>
                 <Typography variant="h1">Oops...</Typography>
                 <Typography variant="p">Page not found!</Typography>
                 <Link href="/">Go back</Link>
-            </LayoutWrapper>
+            </div>
     )
 }
 
