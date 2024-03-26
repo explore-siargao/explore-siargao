7c60de2b508e38cdc2329487896926023ab93a1d
Ramil L. Kaharian <ramilkaharian25@gmail.com>
2024-03-26 10:13:55 +0800

    feat(web): Setup localStore for account state

--- apps/web/common/helpers/workspace.ts
+++ apps/web/common/helpers/workspace.ts
@@ -1,7 +1,10 @@
 import toast from "react-hot-toast"
-
-export const setWorkspace = (role: string) => {
-  if (role === "host" || role === "guest") {
+export enum  WorkSpaceEnum  {
+    HOST="host",
+    GUEST="guest"
+}
+export const setWorkspace = (role: WorkSpaceEnum) => {
+  if (role === WorkSpaceEnum.HOST || role === WorkSpaceEnum.GUEST) {
     localStorage.setItem("workspace", role)
   } else {
     toast.error("Invalid role. Please specify 'host' or 'guest'.")
