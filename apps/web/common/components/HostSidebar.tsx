b2fddc59e1369ec1f1dabd0070e395298a735c82
Richard F. Dela Peña <richard.delapena19@gmail.com>
2024-03-26 09:25:51 +0800

    Created Hosting Sidebar

---
+++ apps/web/common/components/HostSidebar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+
+interface IHostSidebar {
+  title: string
+  icon: JSX.Element
+  link: string
+}
+
+interface HostSidebarProps {
+  navigation: IHostSidebar[]
+}
+
+const HostSideBar = ({ navigation }: HostSidebarProps) => {
+  return (
+    <div className="mt-14 w-2/12  p-5">
+      <nav className="flex flex-1 flex-col">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" className="-mx-2 space-y-1">
+              {navigation.map((item) => (
+                <li key={item.title}>
+                  <Link
+                    href={item.link}
+                    passHref={true}
+                    className="text-gray-600 hover:text-black hover:bg-primary-500 
+                    group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                  >
+                    {item.icon}
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default HostSideBar
