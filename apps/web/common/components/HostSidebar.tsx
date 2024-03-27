a418234b44557f11f9330667c5005cfb65dfdf70
Andal <arjayandal93@gmail.com>
2024-03-27 10:40:41 +0800

    feat(web): Combined Header and Sidebar

--- apps/web/common/components/HostSidebar.tsx
+++ apps/web/common/components/HostSidebar.tsx
@@ -1,31 +1,31 @@
-import Link from "next/link";
-import { Typography } from "./ui/Typography";
+import Link from "next/link"
+import { Typography } from "./ui/Typography"
 
 interface TopLinks {
-  title: string;
-  icon: JSX.Element;
-  link: string;
-  isSelected?: boolean;
+  title: string
+  icon: JSX.Element
+  link: string
+  isSelected?: boolean
 }
 
 interface BottomLinks {
-  title: string;
-  link: string;
+  title: string
+  link: string
 }
 
 interface HostSidebarProps {
-  topLinks: TopLinks[];
-  bottomLinks: BottomLinks[];
-  children: React.ReactNode;
+  topLinks: TopLinks[]
+  bottomLinks: BottomLinks[]
+  children: React.ReactNode
 }
 
 const HostSideBar = ({ topLinks, bottomLinks, children }: HostSidebarProps) => {
   return (
     <div className="grid grid-cols-12">
       <div className="col-span-12 md:col-span-2 md:pt-6">
-        <div className="w-full md:w-2/12 h-screen pl-1 pt-20 mt-5 md:mt-0 pr-1 pb-8 md:fixed md:h-full">
-          <nav className="flex flex-col h-full justify-between">
-            <ul className="space-y-1">
+        <div className=" md:w-2/12 md:h-screen pl-1 pt-20 mt-5 md:mt-0 pr-1 md:pb-8 md:fixed">
+          <nav className="md:flex md:flex-col md:h-full justify-between flex">
+            <ul className="lg:space-x-0 lg:space-y-1 flex flex-wrap md:flex-col justify-between">
               {topLinks.map((item) => (
                 <li
                   key={item.title}
@@ -38,7 +38,7 @@ const HostSideBar = ({ topLinks, bottomLinks, children }: HostSidebarProps) => {
                     href={item.link}
                     passHref={true}
                     className="text-gray-600 hover:text-black hover:bg-primary-400 
-                      group flex gap-x-1 rounded-md p-2 text-sm leading-6 font-semibold"
+            group flex gap-x-1 rounded-md p-2 text-sm leading-6 font-semibold"
                   >
                     {item.icon}
                     <Typography variant="p" fontWeight="semibold">
@@ -48,7 +48,7 @@ const HostSideBar = ({ topLinks, bottomLinks, children }: HostSidebarProps) => {
                 </li>
               ))}
             </ul>
-            <ul className="hidden md:block">
+            <ul className="hidden md:flex md:flex-col">
               <Typography
                 className="ml-1 text-gray-600 text-sm"
                 variant="p"
@@ -62,7 +62,7 @@ const HostSideBar = ({ topLinks, bottomLinks, children }: HostSidebarProps) => {
                     href={item.link}
                     passHref={true}
                     className="text-gray-600 hover:text-black hover:underline 
-                      group flex gap-x-3 rounded-md p-1 text-sm leading-6 font-semibold"
+                    group flex gap-x-3 rounded-md p-1 text-sm leading-6 font-semibold"
                   >
                     <Typography variant="p" fontWeight="semibold">
                       {item.title}
@@ -74,9 +74,11 @@ const HostSideBar = ({ topLinks, bottomLinks, children }: HostSidebarProps) => {
           </nav>
         </div>
       </div>
-      <div className="col-span-12 md:col-span-10 md:pl-0 sm:pl-4 mb-0">{children}</div>
+      <div className="col-span-12 lg:top-0 md:col-span-10 md:pl-0 sm:pl-4 mb-0 absolute top-24 right-0 p-1 w-full lg:w-auto">
+        {children}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default HostSideBar;
+export default HostSideBar
